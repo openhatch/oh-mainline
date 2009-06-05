ad22e83bb27f70657fb8368f50eb3b0b54a0fb6b
Raphael Krut-Landau <raphael.kl@gmail.com>
2009-06-05 14:13:12 -0400

    Converted my devs/index.html into mysite/search/templates/search/search.html, moved related files. The page now loads all bugs in the database.

---
+++ mysite/static/js/devs.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	 $("#button").click(function() {
+		query = ($('#query').val());
+		Opps.fetchOppsToDOM(query);
+		});
+	 });
+
+Opps = {
+	'oppsQueryJSONURL': "http://renaissance.local:8000/search/query_json/",
+	'$oppsDOMList': $('#opps ul'),
+	'fetchOppsToDOM': function (query) {
+	  $('#opps li').remove();
+	  url = this.oppsQueryJSONURL + encodeURIComponent(query) + "?jsoncallback=?";
+	  $.getJSON(url, this.jsonArrayToDocument);
+	},
+	'jsonArrayToDocument': function (jsonArray) {
+	  console.log(jsonArray);
+	  $(jsonArray).each( function() {
+			$opp = $("<li>");
+			$opp.addClass(this.model);
+			$opp.attr('id', "opp-" + this.pk);
+
+			// Heading
+			$h3 = $("<h3>");
+			$title = $("<span>").text(this.fields.title);
+			$h3.append($title);
+			$opp.append($h3);
+
+			// Summary
+			$summary = $("<p>").text(this.fields.description);
+			$opp.append($summary);
+
+			console.log($opp[0]);
+
+			// Add to DOM
+			$('#opps ul').append($opp);
+			console.log(Opps);
+
+			});
+	}
+}
+
+$("#opps ul");
+
+/* vim:set sw=2 ts=3: */
