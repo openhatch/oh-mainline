bb8af79693270a815f83ac4be2eb1a7d468ab4d8
Asheesh Laroia <paulproteus@renaissance.(none)>
2009-06-05 00:20:21 -0400

    Provide a JSON version of the query response

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.core import serializers
 from mysite.search.models import Bug
 
 def query(request, query):
@@ -7,6 +9,13 @@ def query(request, query):
     bunch_of_bugs = Bug.objects.filter(
         project__language=query)
     return render_to_response('search/search.html', {'bunch_of_bugs': bunch_of_bugs})
+
+def query_json(request, query):
+    # FIXME: Give bugs some date field
+    bunch_of_bugs = Bug.objects.filter(
+        project__language=query)
+    json_serializer = serializers.get_serializer('json')()
+    return HttpResponse(json_serializer.serialize(bunch_of_bugs))
 
 def index(request):
     return render_to_response('search/index.html')
