09865cc9f4d1558b86427669b9caa73c2c6bcae0
Asheesh Laroia <paulproteus@renaissance.(none)>
2009-06-04 23:35:42 -0400

    Add some (trivial) models for bug search

--- mysite/search/models.py
+++ mysite/search/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    language = models.CharField(max_length=200)
+
+class Bug(models.Model):
+    project = models.ForeignKey(Project)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.CharField(max_length=200)
+
