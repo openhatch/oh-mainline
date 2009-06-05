bbf61c64ea601cb12035fcd54645fd99a8d13d4c
Asheesh Laroia <paulproteus@renaissance.(none)>
2009-06-05 00:39:53 -0400

    Give the project name in the project field of the JSON

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -2,7 +2,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.core import serializers
-from mysite.search.models import Bug
+from mysite.search.models import Bug, Project
+import simplejson
 
 def query(request, query):
     # FIXME: Give bugs some date field
@@ -14,8 +15,12 @@ def query_json(request, query):
     # FIXME: Give bugs some date field
     bunch_of_bugs = Bug.objects.filter(
         project__language=query)
-    json_serializer = serializers.get_serializer('json')()
-    return HttpResponse(json_serializer.serialize(bunch_of_bugs))
+    json_serializer = serializers.get_serializer('python')()
+    data = json_serializer.serialize(bunch_of_bugs)
+    for elt in data:
+        elt['fields']['project'] = Project.objects.get(pk=int(elt['fields']['project'])).name
+    jsonned = simplejson.dumps(data)
+    return HttpResponse(jsonned)
 
 def index(request):
     return render_to_response('search/index.html')
