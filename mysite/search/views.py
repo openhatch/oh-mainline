e41855b06faa9e2a6327094f8a26b976f5081da2
Raphael Krut-Landau <raphael@openhatch.org>
2009-06-05 19:25:26 -0400

    For pagination, I've added "start_at" and "how_many" to the query method in views.py.

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -5,20 +5,23 @@ from django.core import serializers
 from mysite.search.models import Bug, Project
 import simplejson
 
-def query(request, query):
+def query(request, query, format='html', start_at=0, how_many=10):
     # FIXME: Give bugs some date field
     bunch_of_bugs = Bug.objects.filter(
-        project__language=query)
-    return render_to_response('search/search.html', {'bunch_of_bugs': bunch_of_bugs})
+			project__language=query)[start_at:start_at+how_many]
+	return render_to_response('search/search.html', \
+			{'bunch_of_bugs': bunch_of_bugs, \
+			'start_at': start_at, 'how_many': how_many})
 
-def query_json(request, query):
+def bugs_to_json(request, query):
     # FIXME: Give bugs some date field
     bunch_of_bugs = Bug.objects.filter(
         project__language=query)[:10]
     json_serializer = serializers.get_serializer('python')()
     data = json_serializer.serialize(bunch_of_bugs)
     for elt in data:
-        elt['fields']['project'] = Project.objects.get(pk=int(elt['fields']['project'])).name
+        elt['fields']['project'] = \
+				Project.objects.get(pk=int(elt['fields']['project'])).name
     jsonned = simplejson.dumps(data)
     return HttpResponse(request.GET.get('jsoncallback', 'alert') + '(' + jsonned + ')' )
 
