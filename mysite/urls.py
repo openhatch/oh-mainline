b4aa102b17627a94619dd6eb2cad3a6fc5ee5503
Asheesh Laroia <paulproteus@renaissance.(none)>
2009-06-04 23:46:56 -0400

    Refer to nonexistent (yet) views

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^search/$', 'mysite.search.views.index'),
+    (r'^search/query/(?P<query>\w+)/$', 'mysite.search.views.query'),
+    (r'^search/query_json/(?P<query>\w+)/$', 'mysite.search.views.query_json'),
+    (r'^admin/(.*)', admin.site.root),
+)
+                       
     # Example:
     # (r'^mysite/', include('mysite.foo.urls')),
 
