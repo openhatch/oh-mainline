9547b590d1e65274e1cb0da8ccc90d630b2a366d
Asheesh Laroia <paulproteus@renaissance.(none)>
2009-06-04 23:58:01 -0400

    Add a trivial template

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from mysite.search.models import Bug
+
+def index(request):
+    # FIXME: Give bugs some date field
+    bunch_of_bugs = Bug.objects.all().order_by('title')[:5]
+    return render_to_response('search/index.html', {'bunch_of_bugs': bunch_of_bugs})
--- mysite/urls.py
+++ mysite/urls.py
@@ -10,14 +10,3 @@ urlpatterns = patterns('',
     (r'^search/query_json/(?P<query>\w+)/$', 'mysite.search.views.query_json'),
     (r'^admin/(.*)', admin.site.root),
 )
-                       
-    # Example:
-    # (r'^mysite/', include('mysite.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
-)
