16eddf841045d3f551312e18059f831b86a85ec1
Raphael Krut-Landau <raphael.kl@gmail.com>
2009-06-05 13:51:29 -0400

    Switched to renaissance.local

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysq
 DATABASE_NAME = 'oh_milestone_a'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'oh_milestone_a'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'ahmaC0Th'         # Not used with sqlite3.
-DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_HOST = 'renaissance.local'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
