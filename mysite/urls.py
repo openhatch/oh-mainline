b6c44fb951950c72e09004b4478e497e8dcfa2b0
Asheesh Laroia <paulproteus@renaissance.(none)>
2009-06-04 23:59:33 -0400

    Disable broken ("for now") views

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^search/$', 'mysite.search.views.index'),
-    (r'^search/query/(?P<query>\w+)/$', 'mysite.search.views.query'),
-    (r'^search/query_json/(?P<query>\w+)/$', 'mysite.search.views.query_json'),
+#   (r'^search/query/(?P<query>\w+)/$', 'mysite.search.views.query'),
+#    (r'^search/query_json/(?P<query>\w+)/$', 'mysite.search.views.query_json'),
     (r'^admin/(.*)', admin.site.root),
 )
