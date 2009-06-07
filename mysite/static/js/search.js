12333e83c50bb6ee8a0ab14c7e2311fd5e0f3101
Raphael Krut-Landau <raphael@openhatch.org>
2009-06-07 15:47:20 -0400

    If my calculations are correct, this now flippin rocks, yo. Basically, the bug results page emulates a Gmail conversation, in that you see just quick summaries, and when you click on a title, it expands to show more. Booyah.

--- mysite/static/js/search.js
+++ mysite/static/js/search.js
@@ -1,45 +1,50 @@
-        function update(queryArray) {
-            queryArray.push({'name': 'format', value: 'json'});
+function update(queryArray) {
+    queryArray.push({'name': 'format', value: 'json'});
 
-            queryStringFormatJSON = $.param(queryArray);
+    queryStringFormatJSON = $.param(queryArray);
 
-            /* Fetch JSON and put in DOM. */
-            Opps.fetchOppsToDOM(queryStringFormatJSON);
+    /* Fetch JSON and put in DOM. */
+    Opps.fetchOppsToDOM(queryStringFormatJSON);
 
-            /* Update navigation links */
-            var language;
-            $(queryArray).each(function () {
-                if(this.name == 'language') language = this.value;
-                })
+    /* Update navigation links */
+    var language;
+    $(queryArray).each(function () {
+            if(this.name == 'language') language = this.value;
+            })
 
-            console.log(thisstart, thisend);
+    console.log(thisstart, thisend);
 
-            diff = thisend - thisstart;
+    diff = thisend - thisstart;
 
-            prevPageQueryArray = $('form').serializeArray();
-	    prevPageQueryArray.push( {'name': 'start', 'value': thisstart - diff - 1});
-	    prevPageQueryArray.push( {'name': 'end', 'value': thisstart - 1});
+    prevPageQueryArray = $('form').serializeArray();
+    prevPageQueryArray.push( {'name': 'start', 'value': thisstart - diff - 1});
+    prevPageQueryArray.push( {'name': 'end', 'value': thisstart - 1});
 
 
-            nextPageQueryArray = $('form').serializeArray();
-	    nextPageQueryArray.push( {'name': 'start', 'value': thisend + 1});
-	    nextPageQueryArray.push( {'name': 'end', 'value': thisend + diff + 1});
+    nextPageQueryArray = $('form').serializeArray();
+    nextPageQueryArray.push( {'name': 'start', 'value': thisend + 1});
+    nextPageQueryArray.push( {'name': 'end', 'value': thisend + diff + 1});
 
-            /* Update navigation links to reflect new query. */
-            prefix = '/search/?';
-            $('#prev-page').attr('href', prefix + $.param(prevPageQueryArray));
-            $('#next-page').attr('href', prefix + $.param(nextPageQueryArray));
+    /* Update navigation links to reflect new query. */
+    prefix = '/search/?';
+    $('#prev-page').attr('href', prefix + $.param(prevPageQueryArray));
+    $('#next-page').attr('href', prefix + $.param(nextPageQueryArray));
 
-            $('#results-summary-language').text(language);
-            $('#results-summary-start').text(thisstart);
-            $('#results-summary-end').text(thisend);
-            
-            console.log(thisstart);
-            console.log(thisend);
-            return false;
-        };
+    $('#results-summary-language').text(language);
+    $('#results-summary-start').text(thisstart);
+    $('#results-summary-end').text(thisend);
+
+    console.log(thisstart);
+    console.log(thisend);
+    return false;
+};
 
 $(document).ready(function() {
+
+        $('.first-line .title a').click(function () {
+            $(this.parentNode.parentNode.parentNode.parentNode).toggleClass('expanded');
+            return false;
+            });
 
         /* Takes a query and updates the page. */
         $("#button").click(function() {
@@ -49,28 +54,28 @@ $(document).ready(function() {
 
 
         $('#prev-page, #next-page').click(function() {
-                /* Take the HREF and convert to wacky serializeArray, send to update() */
-		var fruitySerialized = new Array();
-                var splitted_on_ampersands = this.href.split('?')[1].split('&');
-                for (var index in splitted_on_ampersands) {
-		    var splitted = splitted_on_ampersands[index].split('=');    
-		    var key = decodeURIComponent(splitted[0]);
-		    var value = decodeURIComponent(splitted[1]);
-		    var fruity_pushable = {'name': key, 'value': value};
-		    /* update the thisstart and thisend globals */
-		    if (key == 'start') {
-			thisstart = parseInt(value);
-		    }
-		    if (key == 'end') {
-			thisend = parseInt(value);
-		    }
-		    fruitySerialized.push(fruity_pushable);
-		}
-		console.log('FRUITYSERIALIZED:');
-		console.log(fruitySerialized);
-        console.log('SLASH FRUITYSERIALIZED:');
+            /* Take the HREF and convert to wacky serializeArray, send to update() */
+            var fruitySerialized = new Array();
+            var splitted_on_ampersands = this.href.split('?')[1].split('&');
+            for (var index in splitted_on_ampersands) {
+            var splitted = splitted_on_ampersands[index].split('=');    
+            var key = decodeURIComponent(splitted[0]);
+            var value = decodeURIComponent(splitted[1]);
+            var fruity_pushable = {'name': key, 'value': value};
+            /* update the thisstart and thisend globals */
+            if (key == 'start') {
+            thisstart = parseInt(value);
+            }
+            if (key == 'end') {
+            thisend = parseInt(value);
+            }
+            fruitySerialized.push(fruity_pushable);
+            }
+            console.log('FRUITYSERIALIZED:');
+            console.log(fruitySerialized);
+            console.log('SLASH FRUITYSERIALIZED:');
 
-        return update(fruitySerialized);
+            return update(fruitySerialized);
         });
 
 });
@@ -90,10 +95,8 @@ Opps = {
                 $opp.find('.project').text(this.fields.project);
                 $opp.find('.title').text(this.fields.title);
                 $opp.find('.description').text(this.fields.description);
-        });
+                });
     }
 }
-
-$("#opps ul");
 
 /* vim:set sw=4 ts=4 expandtab: */
