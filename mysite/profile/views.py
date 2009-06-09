7b099c73f0e01b9d889bb29172a2156b582f33e4
Asheesh Laroia <asheesh@openhatch.org>
2009-06-09 19:00:15 -0400

    Use a template

--- mysite/profile/views.py
+++ mysite/profile/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
 def index(request):
-    return HttpResponse('')
+    return render_to_response('profile/index.html')
 
