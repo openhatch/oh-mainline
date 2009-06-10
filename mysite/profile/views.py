8588fe9269e8aee5458391dbcea69a14b283670d
Asheesh Laroia <asheesh@openhatch.org>
2009-06-09 23:13:29 -0400

    Verify that adding a contribution changes the next page

--- mysite/profile/views.py
+++ mysite/profile/views.py
@@ -2,4 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('profile/index.html')
+    project = request.GET.get('project', '')
+    contrib_text = request.GET.get('contrib_text', '')
+    url = request.GET.get('url', '')
+
+    if project and contrib_text and url:
+        display_data = dict(project=project,
+                            url=url,
+                            contrib_text=contrib_text)
+    else:
+        display_data = {}
+    
+    return render_to_response('profile/index.html',
+                              display_data)
