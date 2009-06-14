8071f36a8407ba454676bfb0f409b6a6a5e82194
Raphael Krut-Landau <raphael@openhatch.org>
2009-06-14 15:58:38 -0400

    Added explanatory text.

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,18 +2,17 @@ from django.conf.urls.defaults import *
 
 import settings
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^search/$', 'mysite.search.views.fetch_bugs'),
-    (r'^admin/(.*)', admin.site.root),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': settings.STATIC_DOC_ROOT}),
-    (r'^$', 'mysite.search.views.fetch_bugs'),
-    (r'^profile/$', 'mysite.profile.views.index'),
-    (r'^profile/add_contribution$', 'mysite.profile.views.add_contribution'),
-)
+        (r'^$', 'mysite.search.views.fetch_bugs'),
+        (r'^search/$', 'mysite.search.views.fetch_bugs'),
+        (r'^admin/(.*)', admin.site.root),
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.STATIC_DOC_ROOT}),
+        (r'^profile/$', 'mysite.profile.views.index'),
+        (r'^profile/add_contribution$', 'mysite.profile.views.add_contribution'),
+        )
 
-
+# vim: set ai ts=4 sts=4 et sw=4:
