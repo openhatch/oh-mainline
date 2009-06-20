9dcdd75f35c3fe87e2d5d8b83ee5785ac45e6c0f
Asheesh Laroia <asheesh@openhatch.org>
2009-06-20 14:24:58 -0400

    Add migration

---
+++ mysite/profile/migrations/0014_asheesh_add_interested_in_field_to_person.py
@@ -0,0 +1,102 @@
+
+from south.db import db
+from django.db import models
+from profile.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Adding model 'Link_Project_Tag'
+        db.create_table('profile_link_project_tag', (
+            ('id', models.AutoField(primary_key=True)),
+            ('tag', models.ForeignKey(orm.Tag)),
+            ('project', models.ForeignKey(orm['search.Project'])),
+            ('time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 20, 14, 24, 45, 825148))),
+            ('source', models.CharField(max_length=200)),
+        ))
+        db.send_create_signal('profile', ['Link_Project_Tag'])
+        
+        # Adding field 'Person.interested_in_working_on'
+        db.add_column('profile_person', 'interested_in_working_on', models.CharField(default='', max_length=1024))
+        
+        # Changing field 'Link_ProjectExp_Tag.time_record_was_created'
+        db.alter_column('profile_link_projectexp_tag', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 20, 14, 24, 45, 737449)))
+        
+        # Changing field 'Person.time_record_was_created'
+        db.alter_column('profile_person', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 20, 14, 24, 45, 527201)))
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Deleting model 'Link_Project_Tag'
+        db.delete_table('profile_link_project_tag')
+        
+        # Deleting field 'Person.interested_in_working_on'
+        db.delete_column('profile_person', 'interested_in_working_on')
+        
+        # Changing field 'Link_ProjectExp_Tag.time_record_was_created'
+        db.alter_column('profile_link_projectexp_tag', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 19, 22, 10, 35, 705104)))
+        
+        # Changing field 'Person.time_record_was_created'
+        db.alter_column('profile_person', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 19, 22, 10, 35, 395361)))
+        
+    
+    
+    models = {
+        'profile.person': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'interested_in_working_on': ('models.CharField', [], {'default': "''", 'max_length': '1024'}),
+            'last_polled': ('models.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'last_touched': ('models.DateTimeField', [], {'null': 'True'}),
+            'name': ('models.CharField', [], {'max_length': '200'}),
+            'password_hash_md5': ('models.CharField', [], {'max_length': '200'}),
+            'poll_on_next_web_view': ('models.BooleanField', [], {'default': 'True'}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 6, 20, 14, 24, 45, 875695)'}),
+            'username': ('models.CharField', [], {'max_length': '200'})
+        },
+        'profile.tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'tag_type': ('models.ForeignKey', ["orm['profile.TagType']"], {}),
+            'text': ('models.CharField', [], {'max_length': '50'})
+        },
+        'profile.link_projectexp_tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'project_exp': ('models.ForeignKey', ["orm['profile.ProjectExp']"], {}),
+            'source': ('models.CharField', [], {'max_length': '200'}),
+            'tag': ('models.ForeignKey', ["orm['profile.Tag']"], {}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 6, 20, 14, 24, 45, 984494)'})
+        },
+        'search.project': {
+            '_stub': True,
+            'id': ('models.AutoField', [], {'primary_key': 'True'})
+        },
+        'profile.link_project_tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'project': ('models.ForeignKey', ["orm['search.Project']"], {}),
+            'source': ('models.CharField', [], {'max_length': '200'}),
+            'tag': ('models.ForeignKey', ["orm['profile.Tag']"], {}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 6, 20, 14, 24, 46, 134691)'})
+        },
+        'profile.tagtype': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'name': ('models.CharField', [], {'max_length': '100'}),
+            'prefix': ('models.CharField', [], {'max_length': '20'})
+        },
+        'profile.projectexp': {
+            'description': ('models.TextField', [], {}),
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'last_touched': ('models.DateTimeField', [], {'null': 'True'}),
+            'man_months': ('models.PositiveIntegerField', [], {'null': 'True'}),
+            'person': ('models.ForeignKey', ["orm['profile.Person']"], {}),
+            'person_role': ('models.CharField', [], {'max_length': '200'}),
+            'primary_language': ('models.CharField', [], {'max_length': '200', 'null': 'True'}),
+            'project': ('models.ForeignKey', ["orm['search.Project']"], {}),
+            'source': ('models.CharField', [], {'max_length': '100', 'null': 'True'}),
+            'time_record_was_created': ('models.DateTimeField', [], {'null': 'True'}),
+            'url': ('models.URLField', [], {'max_length': '200', 'null': 'True'})
+        }
+    }
+    
+    complete_apps = ['profile']
