ad63ccb9d74518e65d1f7c2173d0e8fb9038d68e
Asheesh Laroia <asheesh@openhatch.org>
2009-06-21 14:27:37 -0400

    Make project name unique

--- mysite/search/models.py
+++ mysite/search/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique = True)
     language = models.CharField(max_length=200)
     icon_url = models.URLField(max_length=200)
 
