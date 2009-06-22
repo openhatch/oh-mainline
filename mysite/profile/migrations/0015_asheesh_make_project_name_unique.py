a2bd05a943b7b1c6f669b887dfd866f7fb54f815
Asheesh Laroia <asheesh@openhatch.org>
2009-06-22 13:51:07 -0400

    Um, should have published this migration earlier

---
+++ mysite/profile/migrations/0015_asheesh_make_project_name_unique.py
@@ -0,0 +1,89 @@
+
+from south.db import db
+from django.db import models
+from profile.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Changing field 'Person.time_record_was_created'
+        db.alter_column('profile_person', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 21, 12, 45, 31, 878127)))
+        
+        # Changing field 'Link_ProjectExp_Tag.time_record_was_created'
+        db.alter_column('profile_link_projectexp_tag', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 21, 12, 45, 32, 159881)))
+        
+        # Changing field 'Link_Project_Tag.time_record_was_created'
+        db.alter_column('profile_link_project_tag', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 21, 12, 45, 32, 215414)))
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Changing field 'Person.time_record_was_created'
+        db.alter_column('profile_person', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 20, 14, 24, 45, 875695)))
+        
+        # Changing field 'Link_ProjectExp_Tag.time_record_was_created'
+        db.alter_column('profile_link_projectexp_tag', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 20, 14, 24, 45, 984494)))
+        
+        # Changing field 'Link_Project_Tag.time_record_was_created'
+        db.alter_column('profile_link_project_tag', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 20, 14, 24, 46, 134691)))
+        
+    
+    
+    models = {
+        'profile.person': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'interested_in_working_on': ('models.CharField', [], {'default': "''", 'max_length': '1024'}),
+            'last_polled': ('models.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'last_touched': ('models.DateTimeField', [], {'null': 'True'}),
+            'name': ('models.CharField', [], {'max_length': '200'}),
+            'password_hash_md5': ('models.CharField', [], {'max_length': '200'}),
+            'poll_on_next_web_view': ('models.BooleanField', [], {'default': 'True'}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 6, 21, 12, 45, 32, 593402)'}),
+            'username': ('models.CharField', [], {'max_length': '200'})
+        },
+        'profile.tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'tag_type': ('models.ForeignKey', ["orm['profile.TagType']"], {}),
+            'text': ('models.CharField', [], {'max_length': '50'})
+        },
+        'profile.link_projectexp_tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'project_exp': ('models.ForeignKey', ["orm['profile.ProjectExp']"], {}),
+            'source': ('models.CharField', [], {'max_length': '200'}),
+            'tag': ('models.ForeignKey', ["orm['profile.Tag']"], {}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 6, 21, 12, 45, 32, 288061)'})
+        },
+        'search.project': {
+            '_stub': True,
+            'id': ('models.AutoField', [], {'primary_key': 'True'})
+        },
+        'profile.link_project_tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'project': ('models.ForeignKey', ["orm['search.Project']"], {}),
+            'source': ('models.CharField', [], {'max_length': '200'}),
+            'tag': ('models.ForeignKey', ["orm['profile.Tag']"], {}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 6, 21, 12, 45, 32, 536696)'})
+        },
+        'profile.tagtype': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'name': ('models.CharField', [], {'max_length': '100'}),
+            'prefix': ('models.CharField', [], {'max_length': '20'})
+        },
+        'profile.projectexp': {
+            'description': ('models.TextField', [], {}),
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'last_touched': ('models.DateTimeField', [], {'null': 'True'}),
+            'man_months': ('models.PositiveIntegerField', [], {'null': 'True'}),
+            'person': ('models.ForeignKey', ["orm['profile.Person']"], {}),
+            'person_role': ('models.CharField', [], {'max_length': '200'}),
+            'primary_language': ('models.CharField', [], {'max_length': '200', 'null': 'True'}),
+            'project': ('models.ForeignKey', ["orm['search.Project']"], {}),
+            'source': ('models.CharField', [], {'max_length': '100', 'null': 'True'}),
+            'time_record_was_created': ('models.DateTimeField', [], {'null': 'True'}),
+            'url': ('models.URLField', [], {'max_length': '200', 'null': 'True'})
+        }
+    }
+    
+    complete_apps = ['profile']
