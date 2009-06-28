5b9c273342f79e4bf123e650d6efc5f9ab71ea76
Asheesh Laroia <asheesh@openhatch.org>
2009-06-28 14:48:52 -0400

    Adding a migration that properly removes the spurious key

---
+++ mysite/profile/migrations/0021_raffi_remove_uniqueness_constraint_on_project_exp.py
@@ -0,0 +1,117 @@
+
+from south.db import db
+from django.db import models
+from profile.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Changing field 'Person.time_record_was_created'
+        db.alter_column('profile_person', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 28, 14, 36, 26, 853766)))
+        
+        # Changing field 'Link_ProjectExp_Tag.time_record_was_created'
+        db.alter_column('profile_link_projectexp_tag', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 28, 14, 36, 27, 271816)))
+        
+        # Changing field 'Link_Project_Tag.time_record_was_created'
+        db.alter_column('profile_link_project_tag', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 28, 14, 36, 27, 361950)))
+        
+        # Deleting unique_together for [person, project] on ProjectExp.
+        try:
+            db.delete_unique('profile_projectexp', ('person', 'project'))
+        except ValueError:
+            db.execute('alter table profile_projectexp drop key profile_projectexp_person_id_6fe12f6f;')
+    
+    
+    def backwards(self, orm):
+        
+        # Changing field 'Person.time_record_was_created'
+        db.alter_column('profile_person', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 25, 10, 24, 42, 27042)))
+        
+        # Changing field 'Link_ProjectExp_Tag.time_record_was_created'
+        db.alter_column('profile_link_projectexp_tag', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 25, 10, 24, 41, 921862)))
+        
+        # Changing field 'Link_Project_Tag.time_record_was_created'
+        db.alter_column('profile_link_project_tag', 'time_record_was_created', models.DateTimeField(default=datetime.datetime(2009, 6, 25, 10, 24, 41, 441781)))
+        
+        # Creating unique_together for [person, project] on ProjectExp.
+        db.create_unique('profile_projectexp', ('person', 'project'))
+        
+    
+    
+    models = {
+        'profile.person': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'interested_in_working_on': ('models.CharField', [], {'default': "''", 'max_length': '1024'}),
+            'last_polled': ('models.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'last_touched': ('models.DateTimeField', [], {'null': 'True'}),
+            'name': ('models.CharField', [], {'max_length': '200'}),
+            'password_hash_md5': ('models.CharField', [], {'max_length': '200'}),
+            'poll_on_next_web_view': ('models.BooleanField', [], {'default': 'True'}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 6, 28, 14, 36, 28, 64541)'}),
+            'username': ('models.CharField', [], {'max_length': '200'})
+        },
+        'profile.tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'tag_type': ('models.ForeignKey', ["orm['profile.TagType']"], {}),
+            'text': ('models.CharField', [], {'max_length': '50'})
+        },
+        'profile.link_projectexp_tag': {
+            'Meta': {'unique_together': "[('tag','project_exp','source'),]"},
+            'favorite': ('models.BooleanField', [], {'default': 'False'}),
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'project_exp': ('models.ForeignKey', ["orm['profile.ProjectExp']"], {}),
+            'source': ('models.CharField', [], {'max_length': '200'}),
+            'tag': ('models.ForeignKey', ["orm['profile.Tag']"], {}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 6, 28, 14, 36, 27, 969508)'})
+        },
+        'profile.sourceforgeperson': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'username': ('models.CharField', [], {'max_length': '200'})
+        },
+        'profile.link_project_tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'project': ('models.ForeignKey', ["orm['search.Project']"], {}),
+            'source': ('models.CharField', [], {'max_length': '200'}),
+            'tag': ('models.ForeignKey', ["orm['profile.Tag']"], {}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 6, 28, 14, 36, 28, 410014)'})
+        },
+        'profile.sourceforgeproject': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'unixname': ('models.CharField', [], {'max_length': '200'})
+        },
+        'search.project': {
+            '_stub': True,
+            'id': ('models.AutoField', [], {'primary_key': 'True'})
+        },
+        'profile.link_sf_proj_dude_fm': {
+            'Meta': {'unique_together': "[('person','project'),]"},
+            'date_collected': ('models.DateTimeField', [], {}),
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'is_admin': ('models.BooleanField', [], {'default': 'False'}),
+            'person': ('models.ForeignKey', ["orm['profile.SourceForgePerson']"], {}),
+            'position': ('models.CharField', [], {'max_length': '200'}),
+            'project': ('models.ForeignKey', ["orm['profile.SourceForgeProject']"], {})
+        },
+        'profile.tagtype': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'name': ('models.CharField', [], {'max_length': '100'}),
+            'prefix': ('models.CharField', [], {'max_length': '20'})
+        },
+        'profile.projectexp': {
+            'description': ('models.TextField', [], {}),
+            'favorite': ('models.BooleanField', [], {'default': '0'}),
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'last_touched': ('models.DateTimeField', [], {'null': 'True'}),
+            'man_months': ('models.PositiveIntegerField', [], {'null': 'True'}),
+            'person': ('models.ForeignKey', ["orm['profile.Person']"], {}),
+            'person_role': ('models.CharField', [], {'max_length': '200'}),
+            'primary_language': ('models.CharField', [], {'max_length': '200', 'null': 'True'}),
+            'project': ('models.ForeignKey', ["orm['search.Project']"], {}),
+            'source': ('models.CharField', [], {'max_length': '100', 'null': 'True'}),
+            'time_record_was_created': ('models.DateTimeField', [], {'null': 'True'}),
+            'url': ('models.URLField', [], {'max_length': '200', 'null': 'True'})
+        }
+    }
+    
+    complete_apps = ['profile']
