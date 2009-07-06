f160d4f5d56d6fb40a3074bc12b1384fae9701bc
Asheesh Laroia <asheesh@openhatch.org>
2009-07-06 14:23:22 -0400

    Actually create TagType.name attribute in database

---
+++ mysite/profile/migrations/0026_asheesh_add_name_to_tagtype_for_real.py
@@ -0,0 +1,110 @@
+
+from south.db import db
+from django.db import models
+from profile.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Adding field 'TagType.qname'
+        try:
+            db.add_column('profile_tagtype', 'name', models.CharField(max_length=100))
+        except Exception, e:
+            raise_the_exception = True
+            for thing in e.args:
+                if 'Duplicate column' in unicode(thing):
+                    raise_the_exception = False
+            if raise_the_exception:
+                raise
+        
+    def backwards(self, orm):
+        
+        # Deleting field 'TagType.qname'
+        db.delete_column('profile_tagtype', 'name')
+        
+    models = {
+        'profile.person': {
+            'gotten_name_from_ohloh': ('models.BooleanField', [], {'default': 'False'}),
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'interested_in_working_on': ('models.CharField', [], {'default': "''", 'max_length': '1024'}),
+            'last_polled': ('models.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'last_touched': ('models.DateTimeField', [], {'null': 'True'}),
+            'name': ('models.CharField', [], {'max_length': '200'}),
+            'ohloh_grab_completed': ('models.BooleanField', [], {'default': 'False'}),
+            'password_hash_md5': ('models.CharField', [], {'max_length': '200'}),
+            'poll_on_next_web_view': ('models.BooleanField', [], {'default': 'True'}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 7, 6, 14, 20, 5, 155192)'}),
+            'username': ('models.CharField', [], {'max_length': '200'})
+        },
+        'profile.link_person_tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'person': ('models.ForeignKey', ["orm['profile.Person']"], {}),
+            'source': ('models.CharField', [], {'max_length': '200'}),
+            'tag': ('models.ForeignKey', ["orm['profile.Tag']"], {}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 7, 6, 14, 20, 5, 830341)'})
+        },
+        'profile.tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'tag_type': ('models.ForeignKey', ["orm['profile.TagType']"], {}),
+            'text': ('models.CharField', [], {'max_length': '50'})
+        },
+        'profile.link_projectexp_tag': {
+            'Meta': {'unique_together': "[('tag','project_exp','source'),]"},
+            'favorite': ('models.BooleanField', [], {'default': 'False'}),
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'project_exp': ('models.ForeignKey', ["orm['profile.ProjectExp']"], {}),
+            'source': ('models.CharField', [], {'max_length': '200'}),
+            'tag': ('models.ForeignKey', ["orm['profile.Tag']"], {}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 7, 6, 14, 20, 5, 729116)'})
+        },
+        'profile.sourceforgeperson': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'username': ('models.CharField', [], {'max_length': '200'})
+        },
+        'profile.link_project_tag': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'project': ('models.ForeignKey', ["orm['search.Project']"], {}),
+            'source': ('models.CharField', [], {'max_length': '200'}),
+            'tag': ('models.ForeignKey', ["orm['profile.Tag']"], {}),
+            'time_record_was_created': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 7, 6, 14, 20, 5, 617922)'})
+        },
+        'profile.sourceforgeproject': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'unixname': ('models.CharField', [], {'max_length': '200'})
+        },
+        'search.project': {
+            '_stub': True,
+            'id': ('models.AutoField', [], {'primary_key': 'True'})
+        },
+        'profile.link_sf_proj_dude_fm': {
+            'Meta': {'unique_together': "[('person','project'),]"},
+            'date_collected': ('models.DateTimeField', [], {}),
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'is_admin': ('models.BooleanField', [], {'default': 'False'}),
+            'person': ('models.ForeignKey', ["orm['profile.SourceForgePerson']"], {}),
+            'position': ('models.CharField', [], {'max_length': '200'}),
+            'project': ('models.ForeignKey', ["orm['profile.SourceForgeProject']"], {})
+        },
+        'profile.tagtype': {
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'prefix': ('models.CharField', [], {'max_length': '20'}),
+            'qname': ('models.CharField', [], {'max_length': '100'})
+        },
+        'profile.projectexp': {
+            'description': ('models.TextField', [], {}),
+            'favorite': ('models.BooleanField', [], {'default': '0'}),
+            'id': ('models.AutoField', [], {'primary_key': 'True'}),
+            'last_touched': ('models.DateTimeField', [], {'null': 'True'}),
+            'man_months': ('models.PositiveIntegerField', [], {'null': 'True'}),
+            'person': ('models.ForeignKey', ["orm['profile.Person']"], {}),
+            'person_role': ('models.CharField', [], {'max_length': '200'}),
+            'primary_language': ('models.CharField', [], {'max_length': '200', 'null': 'True'}),
+            'project': ('models.ForeignKey', ["orm['search.Project']"], {}),
+            'source': ('models.CharField', [], {'max_length': '100', 'null': 'True'}),
+            'time_record_was_created': ('models.DateTimeField', [], {'null': 'True'}),
+            'url': ('models.URLField', [], {'max_length': '200', 'null': 'True'})
+        }
+    }
+    
+    complete_apps = ['profile']
