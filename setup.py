19cfd849f2d5659f68de59cdcd3b373dc3030293
Asheesh Laroia <asheesh@asheesh.org>
2009-08-04 00:24:54 -0400

    Adding necessary bootstrapping files for buildout.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "mysite",
+    version = "3",
+    packages = ['mysite',],
+    url = 'http://openhatch.org/',
+    license = 'Proprietary',
+
+    install_requires = ['setuptools',
+                       ],
+
+    dependency_links = ['http://linode.openhatch.org/~paulproteus/',
+                        ],
+
+    zip_safe = False,
+    include_package_data = True,
+
+    description = "A website",
+
+    author = 'Asheesh Laroia, Raphael Krut-Landau, Karen Rustad',
+    author_email = 'all@openhatch.org',
+
+    package_dir = {'': 'src'},
+
+)
+
