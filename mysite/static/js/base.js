82a36dd460d4902a9c2d5f0dd370702da1abe6d4
Raphael Krut-Landau <raphael@openhatch.org>
2009-08-06 18:58:30 -0400

    Adds quote marks to terms with spaces.

--- mysite/static/js/base.js
+++ mysite/static/js/base.js
@@ -12,14 +12,31 @@ if (typeof fireunit == "undefined") {
 }
 
 // Thanks to
-// http://stackoverflow.com/questions/237104/javascript-array-containsobj/237176#237176
+// Damir's comment on "Javascript - array.contains(obj)"
+// <http://stackoverflow.com/questions/237104/javascript-array-containsobj/237176#237176>
 Array.prototype.contains = function(obj) {
-  var i = this.length;
-  while (i--) {
-    if (this[i] === obj) {
-      return true;
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
+// Thanks to
+// "Escaping regular expression characters in Javascript"
+// by Simon Willison, posted on 20th January 2006.
+// <http://simonwillison.net/2006/Jan/20/escape/>
+RegExp.escape = function(text) {
+    if (!arguments.callee.sRE) {
+        var specials = [
+            '/', '.', '*', '+', '?', '|',
+            '(', ')', '[', ']', '{', '}', '\\'
+                ];
+        arguments.callee.sRE = new RegExp(
+                '(\\' + specials.join('|\\') + ')', 'g'
+                 );
+                }
+                return text.replace(arguments.callee.sRE, '\\$1');
+                }
