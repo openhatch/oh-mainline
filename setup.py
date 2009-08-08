6ac7385be24b2410a9368124960e425f3277251f
Asheesh Laroia <asheesh@openhatch.org>
2009-08-08 21:54:46 +0000

    Link to new Asheesh-hacked Twill version (oops, wrote cc in the name for no reason)

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
 
     install_requires = ['setuptools',
                         'python-distutils-extra',
+                        'twill==0.9.1-cc',
                        ],
 
     dependency_links = ['http://linode.openhatch.org/~paulproteus/',
