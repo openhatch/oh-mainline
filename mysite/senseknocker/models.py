b04573baa37d29392eb09aac23929c9e334e1c39
Raphael Krut-Landau <raphael@openhatch.org>
2009-08-09 18:04:33 -0400

    Create SenseKnocker.Bug and SenseKnocker.BugForm.

--- mysite/senseknocker/models.py
+++ mysite/senseknocker/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Bug(models.Model):
+    """ A bug in OpenHatch, filed through SenseKnocker. """
+    # {{{
+    #user = models.ForeignKey(User, unique=True)
+    background = models.TextField(
+            verbose_name='What were you doing at the time?')
+    expected_behavior = models.TextField(
+            verbose_name='What did you expect to happen?')
+    actual_behavior = models.TextField(
+            verbose_name='What actually happened?')
+
+    def __unicode__(self):
+        return "<SenseKnocker.Bug pk=%d>" % self.pk
+    # }}}
+
+# vim: set ai ts=4 sw=4 et:
--- mysite/senseknocker/views.py
+++ mysite/senseknocker/views.py
@@ -1,11 +1,17 @@
 from mysite.base.views import view, gimme_json
+from forms import BugForm
 
 @view
 def form(request):
-    return (request, "senseknocker/form.html", {})
+    form = BugForm()
+    return (request, "senseknocker/form.html", {'form': form})
 
 @gimme_json
 def handle_form(request):
+    form = BugForm(request.POST)
+    #form.user = request.user
+    form.save()
+
     # FIXME: Actually handle the form.
     data = [{'success': 1}]
     return data
