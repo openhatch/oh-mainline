a056f27c15d388be15740ea861007a9121c27c2c
Raphael Krut-Landau <raphael@openhatch.org>
2009-08-09 18:28:02 -0400

    Test that we actually create a senseknocker bug.

--- mysite/senseknocker/tests.py
+++ mysite/senseknocker/tests.py
@@ -7,10 +7,16 @@ class Form(TwillTests):
 
     def test_form_post_handler(self):
         client = self.login_with_client()
-        json = client.post('/senseknocker/handle_form', {
-                    'before': 'I was singing "Ave Maria" to a potful of dal.', 
+        bug_data = {'before': 'I was singing "Ave Maria" to a potful of dal.', 
                     'expected_behavior': 'I expected the dal to be stirred.', 
-                    'actual_behavior': 'Instead, burnination.'})
-        # Once we have a bug tracker,
-        # check that the POST handler actually added a bug.
+                    'actual_behavior': 'Instead, burnination.'}
+        json = client.post('/senseknocker/handle_form', bug_data)
+
+        # Check there exists at least one bug with the given characteristics
+        # in the DB. (There can be more than one, hypothechnically.)
+        self.assert_(list(senseknocker.Bug.objects.filter(
+                before=bug_data['before'], 
+                expected_behavior=bug_data['expected_behavior'],
+                actual_behavior=bug_data['actual_behavior']
+                )))
         self.assertEqual(json.content, '[{"success": 1}]')
