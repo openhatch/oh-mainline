381ceac3e17721ad70f1cde36c2e3b6466274c29
Asheesh Laroia <asheesh@asheesh.org>
2009-08-12 16:11:53 -0400

    Remove unused (and wrong) search.controllers

--- mysite/search/tasks/__init__.py
+++ mysite/search/tasks/__init__.py
@@ -3,7 +3,6 @@ from mysite.search.models import Project
 from celery.task import PeriodicTask
 from celery.registry import tasks
 from mysite.search.launchpad_crawl import grab_lp_bugs, lpproj2ohproj
-from mysite.search import controllers
 import mysite.customs.miro
 
 class GrabLaunchpadBugs(PeriodicTask):
