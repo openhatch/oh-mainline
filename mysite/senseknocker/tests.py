77bc7fd7a0926b0db8d6612562439604433074c3
Raphael Krut-Landau <raphael@openhatch.org>
2009-08-15 17:50:34 -0400

    Corrected small error in name of field in senseknocker.forms.Form.

--- mysite/senseknocker/tests.py
+++ mysite/senseknocker/tests.py
@@ -6,7 +6,7 @@ class Form(TwillTests):
 
     def test_form_post_handler(self):
         client = self.login_with_client()
-        bug_data = {'before': 'I was singing "Ave Maria" to a potful of dal.', 
+        bug_data = {'background': 'I was singing "Ave Maria" to a potful of dal.', 
                     'expected_behavior': 'I expected the dal to be stirred.', 
                     'actual_behavior': 'Instead, burnination.'}
         json = client.post('/senseknocker/handle_form', bug_data)
@@ -14,9 +14,11 @@ class Form(TwillTests):
         # Check there exists at least one bug with the given characteristics
         # in the DB. (There can be more than one, hypothechnically.)
         bugs = list(senseknocker.models.Bug.objects.filter(
-            before=bug_data['before'], 
+            background=bug_data['background'], 
             expected_behavior=bug_data['expected_behavior'],
             actual_behavior=bug_data['actual_behavior']
             ))
+        import pdb
+        pdb.set_trace()
         self.assert_(bugs)
         self.assertEqual(json.content, '[{"success": 1}]')
