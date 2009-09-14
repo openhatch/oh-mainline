7b084c7959dedb09cfeba6b75eea9ab79e8768ee
Mister Deployed <deploy@linode.openhatch.org>
2009-09-14 19:27:19 +0000

    Reasonable settings on production site:
    * Require invites
    * Give people 20 invites per user
    * Turn off TEMPLATE_DEBUG explicitly

--- mysite/deployment_settings.py
+++ mysite/deployment_settings.py
@@ -3,3 +3,8 @@ from settings import *
 OHLOH_API_KEY='SXvLaGPJFaKXQC0VOocAg'
 DEBUG=False
 ADMINS=['all@openhatch.org']
+
+INVITE_MODE=True # Enabled on production site
+INVITATIONS_PER_USER=20
+
+TEMPLTE_DEBUG=False
