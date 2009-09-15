baab2698f4dda3190eb62896ccbc7c174dd63113
Mister Deployed <deploy@linode.openhatch.org>
2009-09-15 19:07:09 +0000

    Send broken link emails out; send them from mr_website

--- mysite/deployment_settings.py
+++ mysite/deployment_settings.py
@@ -10,3 +10,9 @@ INVITE_MODE=True # Enabled on production site
 INVITATIONS_PER_USER=20
 
 TEMPLTE_DEBUG=False
+
+EMAIL_SUBJECT_PREFIX='[Kaboom@OH] '
+
+SEND_BROKEN_LINK_EMAILS=True
+MANAGERS=ADMINS
+SERVER_EMAIL='mr_website@linode.openhatch.org'
