935272447c80cb6e8d2406bb3f76a91ee252802b
Asheesh Laroia <asheesh@asheesh.org>
2009-09-16 13:37:16 -0400

    On live site, use memcached

--- mysite/deployment_settings.py
+++ mysite/deployment_settings.py
@@ -16,3 +16,5 @@ EMAIL_SUBJECT_PREFIX='[Kaboom@OH] '
 SEND_BROKEN_LINK_EMAILS=True
 MANAGERS=ADMINS
 SERVER_EMAIL='mr_website@linode.openhatch.org'
+
+CACHE_BACKEND = "memcached://127.0.0.1:11211/?timeout=1"
