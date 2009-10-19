46573025176f874737ca28f484658f33075cb61b
Raphael Krut-Landau <raphael@openhatch.org>
2009-10-19 12:39:39 -0400

    @view no longer references get_personal_data. Moved that function, and tags_dict_for_user, back into profile.views.

--- mysite/base/decorators.py
+++ mysite/base/decorators.py
@@ -12,96 +12,10 @@ from mysite.profile.models import ProjectExp, Link_Person_Tag, Link_Project_Tag,
 def view(func, *args, **kw):
     """Decorator for views."""
     request, template, view_data = func(*args, **kw)
-    data = get_personal_data(request.user.get_profile())
+    data = {} 
     data['the_user'] = request.user
-    data['slug'] = func.__name__
+    data['slug'] = func.__name__ # Account settings uses this.
     data.update(view_data)
     return render_to_response(template, data)
-
-#FIXME: Create a separate function that just passes the data required for displaying the little user bar on the top right to the template, and leaves out all the data required for displaying the large user bar on the left.
-def get_personal_data(person):
-    # {{{
-    project_exps = ProjectExp.objects.filter(person=person)
-    projects = [project_exp.project for project_exp in project_exps]
-    projects_extended = odict({})
-
-    for project in projects:
-        exps_with_this_project = ProjectExp.objects.filter(
-                person=person, project=project)
-        exps_with_this_project_extended = {}
-        for exp in exps_with_this_project:
-            tag_links = Link_ProjectExp_Tag.objects.filter(project_exp=exp)
-            tags_for_this_exp = [link.tag for link in tag_links]
-            exps_with_this_project_extended[exp] = {
-                    'tags': tags_for_this_exp}
-            tags_for_this_project = Link_Project_Tag.objects.filter(
-                    project=project)
-            projects_extended[project] = {
-                    'tags': tags_for_this_project,
-                    'experiences': exps_with_this_project_extended}
-
-            # projects_extended now looks like this:
-    # {
-    #   Project: {
-    #       'tags': [Tag, Tag, ...],
-    #       'experiences': {
-    #               ProjectExp: [Tag, Tag, ...],
-    #               ProjectExp: [Tag, Tag, ...],
-    #               ...
-    #           }
-    #   },
-    #   Project: {
-    #       'tags': [Tag, Tag, ...],
-    #       'experiences': {
-    #               ProjectExp: [Tag, Tag, ...],
-    #               ProjectExp: [Tag, Tag, ...],
-    #               ...
-    #           }
-    #   }
-    # }
-
-    # Asheesh's evil hack
-    for exp in project_exps:
-        links = Link_ProjectExp_Tag.objects.filter(project_exp=exp)
-        for link in links:
-            if link.favorite:
-                link.tag.prefix = 'Favorite: ' # FIXME: evil hack, will fix later
-            else:
-                link.tag.prefix = ''
-
-    interested_in_working_on_list = re.split(r', ', person.interested_in_working_on)
-
-    try:
-        photo_url = person.photo.url
-    except ValueError:
-        photo_url = '/static/images/profile-photos/penguin.png'
-
-    # FIXME: Make this more readable.
-    data_dict = {
-            'person': person,
-            'photo_url': photo_url,
-            'interested_in_working_on_list': interested_in_working_on_list, 
-            'projects': projects_extended,
-            } 
-    data_dict['tags'] = tags_dict_for_person(person)
-    data_dict['tags_flat'] = dict(
-        [ (key, ', '.join([k.text for k in data_dict['tags'][key]]))
-          for key in data_dict['tags'] ])
-
-    data_dict['has_set_info'] = any(data_dict['tags_flat'].values())
-
-    return data_dict
-
-    # }}}
-
-def tags_dict_for_person(person):
-    # {{{
-    ret = collections.defaultdict(list)
-    links = Link_Person_Tag.objects.filter(person=person).order_by('id')
-    for link in links:
-        ret[link.tag.tag_type.name].append(link.tag)
-
-    return ret
-    # }}}
 
 # vim: ai ts=3 sts=4 et sw=4 nu
--- mysite/profile/views.py
+++ mysite/profile/views.py
@@ -43,7 +43,7 @@ from mysite.profile.models import \
         Link_SF_Proj_Dude_FM, Link_Person_Tag, \
         DataImportAttempt
 from mysite.search.models import Project
-from mysite.base.decorators import view 
+from mysite.base.decorators import view, get_default_template_data
 
 # This app
 import forms
@@ -99,13 +99,99 @@ def display_person_web(request, user_to_display__username=None):
     user = get_object_or_404(User, username=user_to_display__username)
     person, was_created = Person.objects.get_or_create(user=user)
 
-    data = {}
+    data = get_personal_data(person)
     data['edit_mode'] = False
     data['editable'] = (request.user == user)
     data['notifications'] = mysite.base.controllers.get_notification_from_request(request)
 
     return (request, 'profile/main.html', data)
 
+    # }}}
+
+#FIXME: Create a separate function that just passes the data required for displaying the little user bar on the top right to the template, and leaves out all the data required for displaying the large user bar on the left.
+def get_personal_data(person):
+    # {{{
+    project_exps = ProjectExp.objects.filter(person=person)
+    projects = [project_exp.project for project_exp in project_exps]
+    projects_extended = odict({})
+
+    for project in projects:
+        exps_with_this_project = ProjectExp.objects.filter(
+                person=person, project=project)
+        exps_with_this_project_extended = {}
+        for exp in exps_with_this_project:
+            tag_links = Link_ProjectExp_Tag.objects.filter(project_exp=exp)
+            tags_for_this_exp = [link.tag for link in tag_links]
+            exps_with_this_project_extended[exp] = {
+                    'tags': tags_for_this_exp}
+            tags_for_this_project = Link_Project_Tag.objects.filter(
+                    project=project)
+            projects_extended[project] = {
+                    'tags': tags_for_this_project,
+                    'experiences': exps_with_this_project_extended}
+
+            # projects_extended now looks like this:
+    # {
+    #   Project: {
+    #       'tags': [Tag, Tag, ...],
+    #       'experiences': {
+    #               ProjectExp: [Tag, Tag, ...],
+    #               ProjectExp: [Tag, Tag, ...],
+    #               ...
+    #           }
+    #   },
+    #   Project: {
+    #       'tags': [Tag, Tag, ...],
+    #       'experiences': {
+    #               ProjectExp: [Tag, Tag, ...],
+    #               ProjectExp: [Tag, Tag, ...],
+    #               ...
+    #           }
+    #   }
+    # }
+
+    # Asheesh's evil hack
+    for exp in project_exps:
+        links = Link_ProjectExp_Tag.objects.filter(project_exp=exp)
+        for link in links:
+            if link.favorite:
+                link.tag.prefix = 'Favorite: ' # FIXME: evil hack, will fix later
+            else:
+                link.tag.prefix = ''
+
+    interested_in_working_on_list = re.split(r', ', person.interested_in_working_on)
+
+    try:
+        photo_url = person.photo.url
+    except ValueError:
+        photo_url = '/static/images/profile-photos/penguin.png'
+
+    # FIXME: Make this more readable.
+    data_dict = {
+            'person': person,
+            'photo_url': photo_url,
+            'interested_in_working_on_list': interested_in_working_on_list, 
+            'projects': projects_extended,
+            } 
+    data_dict['tags'] = tags_dict_for_person(person)
+    data_dict['tags_flat'] = dict(
+        [ (key, ', '.join([k.text for k in data_dict['tags'][key]]))
+          for key in data_dict['tags'] ])
+
+    data_dict['has_set_info'] = any(data_dict['tags_flat'].values())
+
+    return data_dict
+
+    # }}}
+
+def tags_dict_for_person(person):
+    # {{{
+    ret = collections.defaultdict(list)
+    links = Link_Person_Tag.objects.filter(person=person).order_by('id')
+    for link in links:
+        ret[link.tag.tag_type.name].append(link.tag)
+
+    return ret
     # }}}
 
 @login_required
