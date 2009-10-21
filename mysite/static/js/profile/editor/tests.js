4e18dc83f6116be49daccf1f92b069a67a4bf7a0
Raphael Krut-Landau <raphael@openhatch.org>
2009-10-21 14:08:10 -0400

    Started javascript for profile editor tests.

---
+++ mysite/static/js/profile/editor/tests.js
@@ -0,0 +1,6 @@
+message = 'We create PortfolioEntryElements when the event handler is responding to a list of objects from the server that includes a new PortfolioEntry.';
+// FIXME: Come up with a suitable response, matching the description above.
+response = { };
+test = function() {
+};
+fireunit.ok(test(), message);
