a13a51dfe0de76e39c10edfe13990b66dabfb24f
Raphael Krut-Landau <raphael@openhatch.org>
2009-10-21 17:14:51 -0400

    Write a test that shows we need to write a JavaScript function to handle updated Portfolio-related data coming to the page from the server

--- mysite/static/js/profile/editor/tests.js
+++ mysite/static/js/profile/editor/tests.js
@@ -1,6 +1,34 @@
 message = 'We create PortfolioEntryElements when the event handler is responding to a list of objects from the server that includes a new PortfolioEntry.';
 // FIXME: Come up with a suitable response, matching the description above.
-response = { };
+response = {
+    // FIXME: We might not need these models here.
+    'dias': [{'pk': 0}],
+    'citations': [{'pk': 0, 'fields': {'portfolio_entry_id': 0}}],
+    'portfolio_entries': [{'pk': 0, 'fields': {'project_id': 0}}],
+    'projects': [{'pk': 0, 'fields': {'name': 'bindlestiff'}}],
+    'summaries': {'0': 'garglefoot'}
+};
+
 test = function() {
+    updatePortfolio(response);
+    $(response['portfolio_entries']).each( function() {
+            // "this" is a JSON representation of a PortfolioEntry.
+            // Did we create this PortfolioEntryElement?
+            fireunit.ok(
+                $('#portfolio_entry_element_' + this.pk).size()==1,
+                "Expected a portfolio_entry_element corresponding to " + this);
+    });
+    /*
+    for each object:
+        if model=Citation:
+            - try to find the CitationElement on the page
+                - if exists, update the CitationElement with its (possibly) new values
+                - if not, create it with class staging
+
+    for each object:
+        if model=DataImportAttempt:
+            - update the progress bar
+    $('staging').removeClass('staging');
+    */
 };
 fireunit.ok(test(), message);
