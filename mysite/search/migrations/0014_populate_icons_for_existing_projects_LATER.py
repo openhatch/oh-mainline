ef9377f0a08d90d59f8350bbe27ce4a67651eb19
Raphael Krut-Landau <raphael@openhatch.org>
2009-11-05 18:27:21 -0500

    Move python code that populates icons to a later migration.

--- mysite/search/migrations/0014_populate_icons_for_existing_projects_LATER.py
+++ mysite/search/migrations/0014_populate_icons_for_existing_projects_LATER.py
@@ -4,17 +4,23 @@ from django.db import models
 from mysite.search.models import *
 
 class Migration:
-
-    no_dry_run = True
     
     def forwards(self, orm):
         "Write your forwards migration here"
-        pass
-        # Do nothing.
 
+         # Populate icons for all existing projects.
+        for orm_project in orm['search.project'].objects.all():
+            model_project = Project.objects.get(pk=orm_project.pk)
+            print "Populating icon for", model_project
+            model_project.populate_icon_from_ohloh()
+            # We know this is unsafe.
+            # If this fails, it might be because
+            # populate_icon_from_ohloh doesn't exist anymore.
+            model_project.save()
+    
     def backwards(self, orm):
         "Write your backwards migration here"
-        pass
+    
     
     models = {
         'search.bug': {
@@ -36,6 +42,7 @@ class Migration:
         'search.project': {
             'date_icon_was_fetched_from_ohloh': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True'}),
             'icon': ('django.db.models.fields.files.ImageField', [], {'default': 'None', 'max_length': '100', 'null': 'True'}),
+            'icon_for_search_result': ('django.db.models.fields.files.ImageField', [], {'default': 'None', 'max_length': '100', 'null': 'True'}),
             'icon_smaller_for_badge': ('django.db.models.fields.files.ImageField', [], {'default': 'None', 'max_length': '100', 'null': 'True'}),
             'icon_url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
