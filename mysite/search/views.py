b6f6f24c74472b457cfada7c4eb3d4597d5fa41c
Raphael Krut-Landau <raphael@openhatch.org>
2009-11-21 20:43:02 -0500

    The function name is with 'regex', not 'regexp'. Pair programming ftw.

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -57,7 +57,8 @@ def get_bugs_by_query_words(query_words, facets={}):
 
     # Filter
     for word in query_words:
-        whole_word = "[[:<:]]%s[[:>:]]" % mysite.base.controllers.mysql_regexp_escape(word)
+        whole_word = "[[:<:]]%s[[:>:]]" % (
+                mysite.base.controllers.mysql_regex_escape(word))
         bugs = bugs.filter(
             Q(project__language__iexact=word) |
             Q(title__iregex=whole_word) |
