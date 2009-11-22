f50ea125e2e5610b3ed4d02dbf938f738eeffad7
Raphael Krut-Landau <raphael@openhatch.org>
2009-11-21 21:05:17 -0500

    Fix failing test for blog crawl.

--- mysite/customs/tests.py
+++ mysite/customs/tests.py
@@ -378,7 +378,7 @@ Keywords: Torrent unittest""")
         bug = all_bugs[0]
         self.assertEqual(bug.people_involved, 5)
 
-class TestOpenHatchBlogCrawl(django.test.TestCase):
+class BlogCrawl(django.test.TestCase):
     def test_summary2html(self):
         yo_eacute = mysite.customs.feed.summary2html('Yo &eacute;')
         self.assertEqual(yo_eacute, u'Yo \xe9')
@@ -387,8 +387,13 @@ class TestOpenHatchBlogCrawl(django.test.TestCase):
     def test_blog_entries(self, mock_feedparser_parse):
         mock_feedparser_parse.return_value = {
             'entries': [
-                {'summary': 'Yo &eacute;'}]}
+                {
+                    'title': 'Yo &eacute;',
+                    'summary': 'Yo &eacute;'
+                    }]}
         entries = mysite.customs.feed._blog_entries()
+        self.assertEqual(entries[0]['title'],
+                         u'Yo \xe9')
         self.assertEqual(entries[0]['unicode_text'],
                          u'Yo \xe9')
 
