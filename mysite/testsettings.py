b470a4044dfe363b4651a07ede162ad364e5bfd2
Raphael Krut-Landau <raphael@openhatch.org>
2009-11-22 18:44:31 -0500

    Move scripts to special directory.

--- mysite/testsettings.py
+++
@@ -1,7 +0,0 @@
-from settings import *
-
-### HACK
-from socket import gethostname
-if gethostname() in ('renaissance', 'yggdrasil', 'builder', 'vellum') and DEBUG:
-    DEBUG_PROPAGATE_EXCEPTIONS=True
-
