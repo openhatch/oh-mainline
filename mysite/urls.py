27a72c69c1f11d2bab5638d830a000f585ed7af2
Raphael Krut-Landau <raphael@openhatch.org>
2009-11-22 18:56:54 -0500

    Use named url pattern for 'Sign in with a password.'

--- mysite/urls.py
+++ mysite/urls.py
@@ -35,9 +35,9 @@ urlpatterns = patterns('',
         (r'^account/catch-me$', 'mysite.account.views.catch_me'),
         (r'^account/logout/$', 'mysite.account.views.logout'),
 
-        (r'^account/login/$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html'}),
+        (r'^account/login/$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html'}, name='login'),
         (r'^account/login/do$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html'}),
-        (r'^account/login/old$', 'django.contrib.auth.views.login', {'template_name': 'account/login_old.html'}),
+        (r'^account/login/old$', 'django.contrib.auth.views.login', {'template_name': 'account/login_old.html'}, name='login_pwd'),
         (r'^account/signup/do$', 'mysite.account.views.signup_do'),
 
         (r'^account/request_invitation/do$',
