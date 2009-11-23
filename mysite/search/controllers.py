3aebda38bfd71496222039cfb27777c547a76836
Raphael Krut-Landau <raphael@openhatch.org>
2009-11-23 17:22:04 -0500

    Remove facet counts and find projects using SELECT DISTINCT from bugs.

--- mysite/search/controllers.py
+++ mysite/search/controllers.py
@@ -7,11 +7,21 @@ def discover_available_facets(query_words=[]):
     Returns facet2value2count
     """
     bugs = mysite.search.views.get_bugs_by_query_words(query_words)
+
+    if not bugs:
+        return []
+
     # The languages facet is based on the project languages, "for now"
     ret = {}
     ret['Language'] = collections.defaultdict(int)
-    projects = set([bug.project for bug in bugs])
-    for bug in bugs:
-        ret['Language'][bug.project.language] += 1
-    ret['Language'] = dict(ret['Language'])
+    projects = bugs.only('project').distinct()
+    for p in projects:
+        ret['Language'][p.language] = 0 # This is to be used for the count. A a a.
+
+    #for bug in bugs:
+    #   ret['Language'][bug.project.language] += 1
+
+    # convert from defaultdict so items will work in dj templates.
+    ret['Language'] = dict(ret['Language']) 
+
     return ret
