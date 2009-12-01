4724143d7bcb5137e261b03800a3b539ccfaca52
Raphael Krut-Landau <raphael@openhatch.org>
2009-12-01 12:14:38 -0500

    Pass request.user to project view.

--- mysite/project/views.py
+++ mysite/project/views.py
@@ -7,6 +7,7 @@ def project(request, project__name = None):
     p = Project.objects.get(name=project__name)
     return render_to_response('project/project.html',
             {
+                'the_user': request.user,
                 'project': p,
                 'contributors': p.get_contributors()
                 }
