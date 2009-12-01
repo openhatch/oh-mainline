15de2fe886c52f0900deeb519f944d22bb5c6db4
Raphael Krut-Landau <raphael@openhatch.org>
2009-12-01 12:28:48 -0500

    Use the @view decorator to ensure that the project page gets user data.

--- mysite/project/views.py
+++ mysite/project/views.py
@@ -1,14 +1,17 @@
 from mysite.search.models import Project
+import django.template
+import mysite.base.decorators
 
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseServerError
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 
+@mysite.base.decorators.view
 def project(request, project__name = None):
     p = Project.objects.get(name=project__name)
-    return render_to_response('project/project.html',
+    return (request,
+            'project/project.html',
             {
-                'the_user': request.user,
                 'project': p,
                 'contributors': p.get_contributors()
-                }
+                },
             )
