fc2ec517b776fbfbc8d6f8758c7144bf53123079
Raphael Krut-Landau and Asheesh Laroia (pairing) <all@openhatch.org>
2009-12-14 12:17:34 -0500

    Add django-assets to dependencies

--- setup.py
+++ setup.py
@@ -19,12 +19,14 @@ setup(
                         'south==0.6-rc1',
                         'multiprocessing==2.6.2.1-oh',
                         'launchpadlib==1.5.3',
+                        'django-assets',
                        ],
 
     dependency_links = ['http://linode.openhatch.org/~paulproteus/',
                         'http://linode.openhatch.org/~paulproteus/python_apt-0.6.17-py2.6-linux-i686.egg#egg=python-apt',
                         'http://linode.openhatch.org/~paulproteus/invitation-1.0.tar.gz#egg=invitation',
                         'http://linode.openhatch.org/~paulproteus/multiprocessing-2.6.2.1-oh.tar.gz#egg=multiprocessing',
+                        'http://linode.openhatch.org/~paulproteus/django-assets-0.2.tar.gz#egg=django-assets',
                         'http://linode.openhatch.org/~paulproteus/South-0.6_pre-py2.6.egg#egg=south',
                        ],
 
