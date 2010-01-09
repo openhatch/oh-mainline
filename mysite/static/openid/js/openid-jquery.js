3c716062bf69799522b3d76f53638bcbd9b3f9b2
Parker Phinney and Raphael Krut-Landau (pairing) <all@openhatch.org>
2010-01-09 17:49:05 -0500

    Clean up homepage; only use openid loading message for Google and Yahoo.

--- mysite/static/openid/js/openid-jquery.js
+++ mysite/static/openid/js/openid-jquery.js
@@ -119,11 +119,50 @@ var openid = {
 
     var self = this;
     $('.openidProvider').bind("click", function(e) {
-      $('#please_click').html('<p><strong>Please wait while your account provider logs you in&hellip;</strong></p>');
-      $('#openid_btns').html('<img style="text-align: center;" src="/static/images/throbber.gif" />');
-      self.signin(e.target.id);
-      return false;
-    });
+
+            /*
+             * If the user clicked 'Google' or 'Yahoo',
+             * we're about to redirect them to a Google or Yahoo page.
+             * In at least Firefox, this means that the user will be
+             * waiting around on our site for a few seconds while the
+             * external page loads. It's not always terribly clear the
+             * browser is actually doing something during these few seconds,
+             * and users might get the idea that our website is a
+             * load of crap.
+             *
+             * To avoid this situation, let's indicate explicitly
+             * that we are contacting Google/Yahoo.
+             */
+
+            var provider_id = e.target.id;
+            if (provider_id == 'google' || provider_id == 'yahoo') {
+
+                // Display explanatory message.
+                $('#please_click').html(
+                    '<div id="openid_while_u_wait">' +
+                    'Requesting credentials from ' +
+                    providers_large[provider_id].name +
+                    '&hellip;</div>');
+
+                // Remove the other buttons.
+                $('#openid_btns').remove();
+
+                // If the user has recently clicked another provider,
+                // a textfield, labelled, for example, "Enter your Flickr username",
+                // will be visible. Let's remove that.
+                $('#openid_input_area').remove();
+
+                // Remove the "Sign in with a password" link.
+                $('a#sign-in-with-a-password').remove();
+
+                /*
+                 * Note that removing the form itself seems to cause problems.
+                 */
+            }
+
+            self.signin(e.target.id);
+            return false;
+            });
 
     var box_id = this.readCookie();
     if (box_id) {
