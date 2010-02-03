71b9bc71f2ebbb2dab15d6d4f1592844814b5f86
Parker Phinney and Raphael Krut-Landau (pairing) <parker@openhatch.org>
2010-02-03 17:11:51 -0500

    Write unicodify decorator so we can write functions that are sure their input is always a Unicode instance.

--- mysite/base/decorators.py
+++ mysite/base/decorators.py
@@ -17,3 +17,24 @@ def view(func, *args, **kw):
     return render_to_response(template, data)
 
 # vim: ai ts=3 sts=4 et sw=4 nu
+
+@decorator
+def unicodify_strings_when_inputted(func, *args, **kwargs):
+    '''Decorator that makes sure every argument passed in that is
+    a string-esque type is turned into a Unicode object. Does so
+    by decoding UTF-8 byte strings into Unicode objects.'''
+    args_as_list = list(args)
+    # first, *args
+    for i in range(len(args)):
+        arg = args[i]
+        if type(arg) is str:
+            args_as_list[i] = unicode(arg, 'utf-8')
+
+    # then, **kwargs
+    for key in kwargs:
+        arg = kwargs[key]
+        if type(arg) is str:
+            kwargs[key] = unicode(arg, 'utf-8')
+    return func(*args_as_list, **kwargs)
+
+    
