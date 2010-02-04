ae248510ac4849f1c3a06878c36767a6cc750886
Asheesh Laroia <asheesh@openhatch.org>
2010-02-04 15:04:55 -0500

    We successfully index the tags of a person.

--- mysite/profile/search_indexes.py
+++ mysite/profile/search_indexes.py
@@ -5,12 +5,12 @@ import mysite.profile.models
 
 class PersonIndex(indexes.SearchIndex):
     null_document = indexes.CharField(document=True)
-    all_tag_texts = MultiValueField()
+    all_tag_texts = indexes.MultiValueField()
 
     def prepare_null_document(self, person_instance):
         return '' # lollerskates
 
     def prepare_all_tag_texts(self, person_instance):
-        return ['your', 'mom bbq']
+        return person_instance.get_tag_texts_for_map()
 
 site.register(mysite.profile.models.Person, PersonIndex)
