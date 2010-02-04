31c60902c7e09fd01b6b89550df342e5431de961
Asheesh Laroia <asheesh@openhatch.org>
2010-02-04 17:46:21 -0500

    Add a column in the search index for the list of projects.

--- mysite/profile/search_indexes.py
+++ mysite/profile/search_indexes.py
@@ -7,12 +7,16 @@ from django.db.models import Q
 class PersonIndex(indexes.SearchIndex):
     null_document = indexes.CharField(document=True)
     all_tag_texts = indexes.MultiValueField()
+    all_public_projects_exact = indexes.MultiValueField()
 
     def prepare_null_document(self, person_instance):
         return '' # lollerskates
 
     def prepare_all_tag_texts(self, person_instance):
         return person_instance.get_tag_texts_for_map()
+
+    def prepare_all_public_projects_exact(self, person_instance):
+        return list(person_instance.get_list_of_project_names())
 
     def get_queryset(self):
         everybody = mysite.profile.models.Person.objects.all()
