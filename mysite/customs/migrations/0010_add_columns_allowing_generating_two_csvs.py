c4a979ad6e5bb0023bc649a950b1952801b4b457
Asheesh Laroia <asheesh@asheesh.org>
2010-02-08 04:09:06 -0500

    migrations

---
+++ mysite/customs/migrations/0010_add_columns_allowing_generating_two_csvs.py
@@ -0,0 +1,74 @@
+
+from south.db import db
+from django.db import models
+from mysite.customs.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Adding field 'RoundupBugTracker.when_generating_the_keyword_csv_get_only_these_keywords'
+        db.add_column('customs_roundupbugtracker', 'when_generating_the_keyword_csv_get_only_these_keywords', orm['customs.roundupbugtracker:when_generating_the_keyword_csv_get_only_these_keywords'])
+        
+        # Adding field 'RoundupBugTracker.when_generating_the_component_csv_get_only_these_components'
+        db.add_column('customs_roundupbugtracker', 'when_generating_the_component_csv_get_only_these_components', orm['customs.roundupbugtracker:when_generating_the_component_csv_get_only_these_components'])
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Deleting field 'RoundupBugTracker.when_generating_the_keyword_csv_get_only_these_keywords'
+        db.delete_column('customs_roundupbugtracker', 'when_generating_the_keyword_csv_get_only_these_keywords')
+        
+        # Deleting field 'RoundupBugTracker.when_generating_the_component_csv_get_only_these_components'
+        db.delete_column('customs_roundupbugtracker', 'when_generating_the_component_csv_get_only_these_components')
+        
+    
+    
+    models = {
+        'customs.recentmessagefromcia': {
+            'branch': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'committer_identifier': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'module': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'path': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'project_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'time_received': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'customs.roundupbugtracker': {
+            'components': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '50', 'null': 'True'}),
+            'csv_keyword': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '50', 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'include_these_roundup_bug_statuses': ('django.db.models.fields.CharField', [], {'default': "'-1,1,2,3,4,5,6'", 'max_length': '255'}),
+            'my_bugs_are_always_good_for_newcomers': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'my_bugs_concern_just_documentation': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['search.Project']"}),
+            'roundup_root_url': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'when_generating_the_component_csv_get_only_these_components': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '50', 'null': 'True'}),
+            'when_generating_the_keyword_csv_get_only_these_keywords': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '50', 'null': 'True'})
+        },
+        'customs.webresponse': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'response_headers': ('django.db.models.fields.TextField', [], {}),
+            'status': ('django.db.models.fields.IntegerField', [], {}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'url': ('django.db.models.fields.TextField', [], {})
+        },
+        'search.project': {
+            'date_icon_was_fetched_from_ohloh': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True'}),
+            'icon_for_profile': ('django.db.models.fields.files.ImageField', [], {'default': 'None', 'max_length': '100', 'null': 'True'}),
+            'icon_for_search_result': ('django.db.models.fields.files.ImageField', [], {'default': 'None', 'max_length': '100', 'null': 'True'}),
+            'icon_raw': ('django.db.models.fields.files.ImageField', [], {'default': 'None', 'max_length': '100', 'null': 'True'}),
+            'icon_smaller_for_badge': ('django.db.models.fields.files.ImageField', [], {'default': 'None', 'max_length': '100', 'null': 'True'}),
+            'icon_url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'logo_contains_name': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'unique': 'True'})
+        }
+    }
+    
+    complete_apps = ['customs']
