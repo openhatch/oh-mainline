1bc65d9b504ef3ba88ad6b1b1ca470dc27768ff9
Asheesh Laroia <asheesh@openhatch.org>
2010-02-12 13:16:08 -0500

    Stop mocking out Project.get_contributors, and instead store a value in cached_contributors_count.

--- mysite/profile/tests.py
+++ mysite/profile/tests.py
@@ -1762,40 +1762,44 @@ class ImportGithubCollaborators(BaseCeleryTest):
         # }}}
 
 class PeopleSearchProperlyIdentifiesQueriesThatFindProjects(TwillTests):
-    @mock.patch('mysite.search.models.Project.get_contributors')
-    def test_one_valid_project(self, mock_get_contribs):
-        mock_get_contribs.return_value = ['one dude']
+    def test_one_valid_project(self):
         # make a project called Banana
         # query for that, but spelled bANANA
         # look in the template and see that projects_that_match_q_exactly == ['Banana']
-        mysite.search.models.Project.create_dummy(name='Banana')
+        mysite.search.models.Project.create_dummy(
+            name='Banana',
+            cached_contributor_count=1)
         url = reverse(mysite.profile.views.people)
         response = self.client.get(url, {'q': 'bANANA'})
         self.assertEqual(response.context[0]['projects_that_match_q_exactly'],
                          [Project.objects.get(name='Banana')])
         
-    @mock.patch('mysite.search.models.Project.get_contributors')
-    def test_two_valid_projects(self, mock_get_contribs):
-        mock_get_contribs.return_value = ['one dude']
+    def test_two_valid_projects(self):
         # make a project called Banana and Cucumber
         # query for that, but spelled bANANA and cucumBer
         # look in the template and see that set(projects_that_match_q_exactly) == set(['Banana', 'Cucumber'])
-        mysite.search.models.Project.create_dummy(name='Banana')
-        mysite.search.models.Project.create_dummy(name='Cucumber')
-        mysite.search.models.Project.create_dummy(name='Mister Decoy')
+        mysite.search.models.Project.create_dummy(
+            name='Banana',
+            cached_contributor_count=1)
+        mysite.search.models.Project.create_dummy(
+            name='Cucumber',
+            cached_contributor_count=1)
+        mysite.search.models.Project.create_dummy(
+            name='Mister Decoy',
+            cached_contributor_count=1)
         url = reverse(mysite.profile.views.people)
         response = self.client.get(url, {'q': 'bANANA cuCUMBer'})
         self.assertEqual(set(response.context[0]['projects_that_match_q_exactly']),
                          set([Project.objects.get(name='Banana'),
                               Project.objects.get(name='Cucumber')]))
 
-    @mock.patch('mysite.search.models.Project.get_contributors')
-    def test_one_empty_project(self, mock_get_contribs):
-        mock_get_contribs.return_value = [] # nobody!
+    def test_one_empty_project(self):
         # make a project called Banana
         # query for that, but spelled bANANA
         # look in the template and see that projects_that_match_q_exactly == ['Banana']
-        mysite.search.models.Project.create_dummy(name='Banana')
+        mysite.search.models.Project.create_dummy(
+            name='Banana',
+            cached_contributor_count=0)
         url = reverse(mysite.profile.views.people)
         response = self.client.get(url, {'q': 'bANANA'})
         self.assertEqual(response.context[0]['projects_that_match_q_exactly'],
