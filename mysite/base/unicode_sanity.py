332f872db4d2b7dfb3b0e361dab38924675e10cb
Asheesh Laroia <asheesh@openhatch.org>
2010-02-16 12:12:47 -0500

    forgot to git-add, yikes

---
+++ mysite/base/unicode_sanity.py
@@ -0,0 +1,26 @@
+### I will not be afraid of Unicode.
+
+### I do want to monkey-patch away Python stdlib functions
+### that stress me out.
+
+### Sadly our dependencies use them, so I can't.
+import urllib
+
+_urlencode = urllib.urlencode
+
+def urlencode(unicode_dict):
+    utf8_dict = {}
+    bad_keys = []
+    bad_values = []
+    for key in unicode_dict:
+        value = unicode_dict[key]
+        if type(key) == str:
+            bad_keys.append(key)
+        if type(value) == str:
+            bad_values.append(value)
+        utf8_dict[unicode(key).encode('utf-8')] = unicode(value).encode('utf-8')
+    if bad_keys or bad_values:
+        import pdb
+        pdb.set_trace()
+    return _urlencode(utf8_dict)
+    
