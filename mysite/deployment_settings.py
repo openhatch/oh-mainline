389fed9c346cf4eeac01c1c888fdae127ebdbb24
Asheesh Laroia <asheesh@openhatch.org>
2010-03-02 14:02:52 -0500

    Stop mentioning CELERY_ALWAYS_EAGER

--- mysite/deployment_settings.py
+++ mysite/deployment_settings.py
@@ -20,5 +20,3 @@ SERVER_EMAIL='mr_website@linode.openhatch.org'
 CACHE_BACKEND = "memcached://127.0.0.1:11211/?timeout=1"
 
 POSTFIX_FORWARDER_TABLE_PATH = '/etc/postfix/virtual_alias_maps'
-
-CELERY_ALWAYS_EAGER = False
