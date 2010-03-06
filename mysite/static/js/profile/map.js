d0e5cb69746fb9d0e970a2822f5d7acafcf543b6
Parker Phinney <parker.phinney@gmail.com>
2010-03-05 20:37:20 -0500

    bugfix: uninhabited island help text uses all the right conditionals now

--- mysite/static/js/profile/map.js
+++ mysite/static/js/profile/map.js
@@ -52,7 +52,7 @@ PeopleMapController = function () {
     this.explainUninhabitedIsland = function (originatingLink) {
         var message = "People who haven't set their locations appear " + 
             "on an uninhabited island in the South Atlantic.";
-        if(!logged_in_user_location_confirmed){
+        if(logged_in_user_location_unconfirmed){
             message = message +
                 "<br /><br />To set your location, click 'settings' in the top-right " +
                 "corner of your screen.";
