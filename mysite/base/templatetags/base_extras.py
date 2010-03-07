1ed4d153883668b3739a55ee60f2e34280584f01
Asheesh Laroia <asheesh@openhatch.org>
2010-03-07 12:51:01 -0500

    Add some Google Analytics tracking.

--- mysite/base/templatetags/base_extras.py
+++ mysite/base/templatetags/base_extras.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django import template
 from django.utils.timesince import timesince
 import time
@@ -20,3 +21,31 @@ def timesince_terse(value, **args):
     if ret == '0 minutes':
         return 'just moments'
     return ret 
+
+class ShowGoogleAnalyticsJS(template.Node):
+    def render(self, context):
+        code =  getattr(settings, "GOOGLE_ANALYTICS_CODE", False)
+        if not code:
+            return "<!-- Goggle Analytics not included because you haven't set the settings.GOOGLE_ANALYTICS_CODE variable! -->"
+
+        if 'user' in context and context['user'] and context['user'].is_staff:
+            return "<!-- Goggle Analytics not included because you are a staff user! -->"
+
+        if settings.DEBUG:
+            return "<!-- Goggle Analytics not included because you are in Debug mode! -->"
+
+        return """
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("%s");
+pageTracker._trackPageview();
+} catch(err) {}</script>""" % code
+
+def googleanalyticsjs(parser, token):
+    return ShowGoogleAnalyticsJS()
+
+show_common_data = register.tag(googleanalyticsjs)
