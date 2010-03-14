2ffcad78c1dc8afd846f58bba8dd09441b19d10b
Mister Deployed <deploy@linode.openhatch.org>
2010-03-14 20:30:58 +0000

    cache +projects/

--- mysite/project/views.py
+++ mysite/project/views.py
@@ -90,6 +90,9 @@ def project(request, project__name = None):
             'project/project.html',
             context)
 
+from django.views.decorators.cache import cache_page
+@cache_page(60 * 15)
+
 def projects(request):
     data = {}
     query = request.GET.get('q', '')
