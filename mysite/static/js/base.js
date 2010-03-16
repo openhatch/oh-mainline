e97a5c23cd1d79a9ac3e5fa0f94a4bc6d8db086a
Parker Phinney and Raphael Krut-Landau (pairing) <parker@openhatch.org>
2010-03-16 16:36:43 -0400

    wip: add tipsy message to the wannahelp button.

--- mysite/static/js/base.js
+++ mysite/static/js/base.js
@@ -42,4 +42,8 @@ RegExp.escape = function(text) {
                  );
                 }
                 return text.replace(arguments.callee.sRE, '\\$1');
-                }
+};
+
+$(function () {
+    $("a[rel='tipsy']").tipsy({'gravity': 's'});
+});
