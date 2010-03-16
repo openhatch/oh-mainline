38badb93626d9afb855bfec3a89165aed7dd6aba
Parker Phinney and Raphael Krut-Landau (pairing) <parker@openhatch.org>
2010-03-16 17:01:32 -0400

    Iterating on tipsy message for the 'I want to help' button. Message now has an opaquer background.

--- mysite/static/js/base.js
+++ mysite/static/js/base.js
@@ -45,5 +45,8 @@ RegExp.escape = function(text) {
 };
 
 $(function () {
-    $("[rel='tipsy']").tipsy({'gravity': 's'});
+    $("[rel='tipsy-north']").tipsy({'gravity': 'n'});
+    $("[rel='tipsy-east']").tipsy({'gravity': 'e'});
+    $("[rel='tipsy-west']").tipsy({'gravity': 'w'});
+    $("[rel='tipsy-south']").tipsy({'gravity': 's'});
 });
