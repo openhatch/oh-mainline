ecb78d19c3b95c5d07b92adb76e65c7c669e60df
Asheesh Laroia <asheesh@asheesh.org>
2010-03-24 12:21:06 -0400

    A little script for creating a somewhat sanitized data dump.

---
+++ mysite/scripts/clean_data_for_academic_analysis.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+### The purpose of this script is to create a version of the database
+### that helps a U Mass Amherst researcher look through the OpenHatch
+### data and perform text classification and other text analysis.
+
+### To protect our users' privacy, we:
+### * set the password column to the empty string
+### * set the email column to the empty string
+### * delete (from the database) any PortfolioEntry that is_deleted
+### * delete (from the database) any Citation that is_deleted
+### * delete all WebResponse objects
+
+import mysite.profile.models
+import django.contrib.auth.models
+
+### set the email and password columns to the empty string
+for user in django.contrib.auth.models.User.objects.all():
+    user.email = ''
+    user.password = ''
+    user.save()
+
+### delete PortfolioEntry instances that is_deleted
+for pfe in mysite.profile.models.PortfolioEntry.objects.all():
+    if pfe.is_deleted:
+        pfe.delete()
+
+### delete Citation instances that is_deleted
+for citation in mysite.profile.models.Citation.objects.all():
+    if citation.is_deleted:
+        citation.delete()
+
+### delete all WebResponse objects
+for wr in mysite.customs.models.WebResponse.objects.all():
+    wr.delete()
+
