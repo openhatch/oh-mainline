6d929b50ad0d8d58466b05f23889a2e375216d23
John Stumpo <stump@jstump.com>
2010-03-24 11:45:52 -0400

    Unbreak the widget for application/xhtml+xml pages.
    This involves lowercasing the argument to getElementsByTagName and moving
    the <style> out of the widget and into the page header.

--- mysite/base/templates/base/append_ourselves.js
+++ mysite/base/templates/base/append_ourselves.js
@@ -1,7 +1,7 @@
 var in_string = {{ in_string|safe }};
 
 var thisScript = /openhatch-widget.js/;
-var theScripts = document.getElementsByTagName('SCRIPT');
+var theScripts = document.getElementsByTagName('script');
 for (var i = 0 ; i < theScripts.length; i++) {
     if(theScripts[i].src.match(thisScript)) {
         var inForm = false;
@@ -25,8 +25,21 @@ for (var i = 0 ; i < theScripts.length; i++) {
             in_string = in_string.replace('<form ', '<div ');
             in_string = in_string.replace('</form>', '</div>');
         }
-        var my_div = document.createElement('DIV');
+        var my_div = document.createElement('div');
         my_div.innerHTML = in_string;
+
+        /* Hack alert: If we're in an XHTML document being served as
+           application/xhtml+xml, we need to transplant the CSS into
+           the page header for things to work correctly... */
+        var my_style = my_div.getElementsByTagName('style')[0];
+        var css_code = my_style.textContent;
+        my_div.removeChild(my_style);
+
+        var new_style = document.createElement('style');
+        new_style.setAttribute('type', 'text/css');
+        new_style.appendChild(document.createTextNode(css_code));
+        var page_head = document.getElementsByTagName('head')[0];
+        page_head.appendChild(new_style);
 
         theScripts[i].parentNode.insertBefore(my_div, theScripts[i]);
         theScripts[i].parentNode.removeChild(theScripts[i]);
