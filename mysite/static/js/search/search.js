830eeae5cb4bded2553c2fb00baa5007d3d710be
Parker Phinney and Raphael Krut-Landau (pairing) <parker@openhatch.org>
2010-04-08 15:48:40 -0400

    Finish removing dead search ajax code.

--- mysite/static/js/search/search.js
+++ mysite/static/js/search/search.js
@@ -1,14 +1,7 @@
-$.fn.toggleText = function(text1, text2) {
-    newtext = (this.text() == text2) ? text1 : text2;
-    return this.text(newtext);
-};
-
 $.fn.toggleExpanded = function() {
-    //console.log(this, 'toggleExpanded');
     this.toggleClass('expanded');
     return this;
 };
-// Return the available content height space in browser window
 
 SearchResults = {}
 
@@ -40,40 +33,6 @@ SearchResults.bindEventHandlers = function () {
 
 }
 
-SearchResults.shortcutsEnabled = false;
-
-SearchResults.moveSearchResultFocusDown = function () {
-    SearchResults.lightSearchResult(SearchResults.getLitSearchResultIndex() + 1);
-};
-
-SearchResults.moveSearchResultFocusUp = function () {
-    SearchResults.lightSearchResult(SearchResults.getLitSearchResultIndex() - 1);
-};
-
-SearchResults.initializeAutoComplete = function() {
-    $input = $("#opps form input[type='text']");
-    //console.log("input", $input);
-    url = "/search/get_suggestions";
-    acOptions = {
-        'minChars': 1,
-        /*
-           'extraParams': {
-           'partial_query': '',
-           },*/
-        'multiple': true,
-        'multipleSeparator': " ",
-        'matchContains': true
-    };
-    // $input.autocomplete(url, acOptions);
-};
-
-
 $(SearchResults.bindEventHandlers);
-
-$(function() {
-        SearchResults.lightSearchResult(0);
-        });
-
-
 
 /* vim: set ai ts=4 sts=4 et sw=4: */
