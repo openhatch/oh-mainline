2b8914e9cee021bec2f74e39f22daebcecbc0ecf
Asheesh Laroia <asheesh@openhatch.org>
2010-04-09 17:08:36 -0400

    Adding skeletal KDE importer.

---
+++ mysite/customs/bugtrackers/kde.py
@@ -0,0 +1,40 @@
+import logging
+
+import mysite.customs.bugtrackers.bugzilla_general
+
+QUERY_URL='https://bugs.kde.org/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_type=allwords&keywords=junior-jobs&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=REOPENED&bug_status=NEEDSINFO&bug_status=VERIFIED&resolution=---&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0='
+BUG_URL_PREFIX = 'https://bugs.kde.org/show_bug.cgi?id='
+
+def project_finder_plugin(bug_xml_elt):
+    print "EGAD"
+    import mysite.search.models
+    product = bug_xml_elt.xpath('product')[0].text
+    component = bug_xml_elt.xpath('component')[0].text
+    logging.info('Prod ' + product +
+                 'Comp ' + component)
+    project_name = 'KDE' # FOR NOW
+    project, _ = mysite.search.models.Project.objects.get_or_create(
+        name=project_name)
+    return project
+
+def detect_if_good_for_newcomers_plugin(bug_xml_elt, bug_object):
+    keywords_blob = bug_xml_elt.xpath('keywords')[0].text
+    splitted = keywords_blob.split(',')
+    splitted_and_stripped = [k.strip() for k in splitted]
+    if 'junior_jobs' in splitted:
+        bug_object.good_for_newcomers = True
+    else:
+        bug_object.good_for_newcomers = False        
+
+def get_current_bug_id2bug_objs():
+    return mysite.customs.bugtrackers.bugzilla_general.query_url2bug_objects(
+        BUG_URL_PREFIX=BUG_URL_PREFIX,
+        QUERY_URL=QUERY_URL,
+        project_finder_plugin=project_finder_plugin,
+        detect_if_good_for_newcomers_plugin=detect_if_good_for_newcomers_plugin)
+
+def grab():
+    current_bug_id2bug_objs = get_current_bug_id2bug_objs()
+    mysite.customs.bugtrackers.bugzilla_general.grab(
+        current_bug_id2bug_objs,
+        BUG_URL_PREFIX=BUG_URL_PREFIX)
