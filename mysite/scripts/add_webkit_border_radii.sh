de791578c514641ae4ca55826f76cf0b67ec5dc5
Raphael Krut-Landau (driver) and Asheesh Laroia (navigator) <raphael@openhatch.org>
2010-05-19 11:59:31 -0400

    Changes to add webkit border radii script

--- mysite/scripts/add_webkit_border_radii.sh
+++ mysite/scripts/add_webkit_border_radii.sh
@@ -1,1 +1,1 @@
-find . | grep ".*\.css$" | xargs sed -i -e '/.*webkit.*/!s/; -moz-b\([^;]*\);/; -webkit-b\1\0/g' -e 's/-webkit-border-radius-\(top\|bottom\)\(left\|right\)/-webkit-border-\1-\2-radius/g'
+find -name "*.css" -print0 | xargs -0 sed -i -e '/.*webkit.*/!s/-moz-b\([^;]*\);/-webkit-b\1; \0/g' -e 's/-webkit-border-radius-\(top\|bottom\)\(left\|right\)/-webkit-border-\1-\2-radius/g'
