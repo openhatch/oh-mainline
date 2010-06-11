24098935503306106e3347d0eb9ad67c5bf057c3
Raphael Krut-Landau (driver) and Asheesh Laroia (navigator) <raphael@openhatch.org>
2010-06-11 16:29:39 -0400

    Weekly email view is generated with respect to the logged-in user.

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -222,6 +222,7 @@ def landing_for_documenters(request):
     return (request, 'landing_for_documenters.html',
             front_page_data())
 
+@login_required
 @view
 def test_weekly_email_re_projects(request):
     from mysite.profile.management.commands import send_weekly_emails
@@ -229,5 +230,5 @@ def test_weekly_email_re_projects(request):
     command = send_weekly_emails.Command()
     command.this_run_covers_things_since = datetime.datetime(2009, 5, 28)
     command.this_run_covers_things_up_until = datetime.datetime.utcnow()
-    context = command.get_context_for_weekly_email_to(Person.get_by_username('paulproteus'))
+    context = command.get_context_for_weekly_email_to(request.user.get_profile()) or {}
     return (request, 'weekly_email_re_projects.txt', context)
