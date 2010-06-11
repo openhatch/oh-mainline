edeb890f78489ddb1c9eb4d0c731263682f2b599
Asheesh Laroia <asheesh@openhatch.org>
2010-06-11 19:48:16 -0400

    Create a local copy of member.name, hacked to pretend Python 2.5 and 2.6 have tarfile modules that work the same.

--- mysite/missions/controllers.py
+++ mysite/missions/controllers.py
@@ -3,6 +3,16 @@ from django import forms
 import tarfile
 from StringIO import StringIO
 import os
+import sys
+
+def remove_slash_that_python_two_point_five_might_have_added(some_string):
+    if sys.version_info[:2] == (2, 5):
+        # zomg, geez. Let's ditch Python 2.5 real soon now.
+        # Not because it's busted necessarily, but just because we could
+        # stop wasting time on inter-Python-version compatibility code like...
+        if some_string[-1] == '/':
+            return some_string[:-1]
+    return some_string
 
 class IncorrectTarFile(Exception):
     pass
@@ -32,24 +42,26 @@ int main(void)
             raise IncorrectTarFile, 'Archive is not a valid gzipped tarball'
 
         # Check the filename list.
-        filenames_wanted = [cls.WRAPPER_DIR_NAME] + [cls.WRAPPER_DIR_NAME+'/'+filename for filename in cls.FILES.keys()]
+        filenames_wanted = [cls.WRAPPER_DIR_NAME] + [os.path.join(cls.WRAPPER_DIR_NAME, filename) for filename in cls.FILES.keys()]
         for member in tfile.getmembers():
-            if '/' not in member.name:
-                if member.name in cls.FILES.keys():
+            name_as_if_this_were_python_two_point_six = remove_slash_that_python_two_point_five_might_have_added(member.name)
+
+            if '/' not in name_as_if_this_were_python_two_point_six:
+                if name_as_if_this_were_python_two_point_six in cls.FILES.keys():
                     raise IncorrectTarFile, 'No wrapper directory is present'
-                elif member.isdir() and member.name != cls.WRAPPER_DIR_NAME:
-                    raise IncorrectTarFile, 'Wrapper directory name is incorrect: "%s"' % member.name
-            if member.name not in filenames_wanted:
-                raise IncorrectTarFile, 'An unexpected entry "%s" is present' % member.name
-            filenames_wanted.remove(member.name)
-            if member.name == cls.WRAPPER_DIR_NAME:
+                elif member.isdir() and name_as_if_this_were_python_two_point_six != cls.WRAPPER_DIR_NAME:
+                    raise IncorrectTarFile, 'Wrapper directory name is incorrect: "%s"' % name_as_if_this_were_python_two_point_six
+            if name_as_if_this_were_python_two_point_six not in filenames_wanted:
+                raise IncorrectTarFile, 'An unexpected entry "%s" is present' % name_as_if_this_were_python_two_point_six
+            filenames_wanted.remove(name_as_if_this_were_python_two_point_six)
+            if name_as_if_this_were_python_two_point_six == cls.WRAPPER_DIR_NAME:
                 if not member.isdir():
-                    raise IncorrectTarFile, '"%s" should be a directory but is not' % member.name
+                    raise IncorrectTarFile, '"%s" should be a directory but is not' % name_as_if_this_were_python_two_point_six
             else:
                 if not member.isfile():
-                    raise IncorrectTarFile, 'Entry "%s" is not a file' % member.name
-                if tfile.extractfile(member).read() != cls.FILES[member.name.split('/')[-1]]:
-                    raise IncorrectTarFile, 'File "%s" has incorrect contents' % member.name
+                    raise IncorrectTarFile, 'Entry "%s" is not a file' % name_as_if_this_were_python_two_point_six
+                if tfile.extractfile(member).read() != cls.FILES[name_as_if_this_were_python_two_point_six.split('/')[-1]]:
+                    raise IncorrectTarFile, 'File "%s" has incorrect contents' % name_as_if_this_were_python_two_point_six
         if len(filenames_wanted) != 0:
             raise IncorrectTarFile, 'Archive does not contain all expected files (missing %s)' % (', '.join('"%s"' % f for f in filenames_wanted))
 
