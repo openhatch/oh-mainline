e1e7b72685df12d1d7d782e03878253663a4c790
Raphael Krut-Landau (driver) and Asheesh Laroia (navigator) <raphael@openhatch.org>
2010-06-10 22:21:03 -0400

    Fix script that removes numerals from people's locations.

--- mysite/scripts/remove_numbers_from_locations.py
+++ mysite/scripts/remove_numbers_from_locations.py
@@ -5,7 +5,10 @@ Person = mysite.profile.models.Person
 people_with_weird_locations = Person.objects.filter(location_display_name__regex=', [0-9][0-9],')
 
 for p in people_with_weird_locations:
-    location_pieces = re.split(r', \d\d', p.location_display_name)
-    unweirded_location = "".join(location_pieces)
-    p.location_display_name = unweirded_location
-    p.save()
+    location_pieces = re.split(r', \d\d,', p.location_display_name)
+    unweirded_location = ",".join(location_pieces)
+    if unweirded_location != p.location_display_name:
+        print "weird location: " + p.location_display_name
+        p.location_display_name = unweirded_location
+        print "unweirded location: " + p.location_display_name
+        p.save()
