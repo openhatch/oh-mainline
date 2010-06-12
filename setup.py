279bab1685a1b8b6952613e51e6c752551e5099c
Asheesh Laroia <asheesh@asheesh.org>
2010-06-12 14:00:37 -0400

    Hope that Django 1.1.2 is compatible with Python 2.6.5.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     license = 'AGPLv3',
 
     install_requires = ['setuptools',
-                        'multiprocessing',
                         'python-distutils-extra',
                         'twill==0.9.1-cc',
                         'invitation',
