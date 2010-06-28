d6e64633edec14beba0bd443e6c0e8a3f86ad6d0
John Stumpo <stump@jstump.com>
2010-06-28 15:44:00 -0400

    Import the controllers module rather than individual things from it.

--- mysite/missions/views.py
+++ mysite/missions/views.py
@@ -1,6 +1,5 @@
 from mysite.base.decorators import view
-from mysite.missions.controllers import TarMission, IncorrectTarFile, UntarMission, mission_completed, PatchSingleFileMission
-from mysite.missions import forms
+from mysite.missions import forms, controllers
 from mysite.missions.models import Step, StepCompletion
 
 from django.http import HttpResponseRedirect, HttpResponse, Http404
@@ -28,10 +27,10 @@ def tar_upload(request):
         form = forms.TarUploadForm(request.POST, request.FILES)
         if form.is_valid():
             try:
-                TarMission.check_tarfile(form.cleaned_data['tarfile'].read())
+                controllers.TarMission.check_tarfile(form.cleaned_data['tarfile'].read())
                 data['create_success'] = True
                 StepCompletion.objects.get_or_create(person=request.user.get_profile(), step=Step.objects.get(name='tar'))
-            except IncorrectTarFile, e:
+            except controllers.IncorrectTarFile, e:
                 data['what_was_wrong_with_the_tarball'] = str(e)
         data['create_form'] = form
     return tar_mission(request, data)
@@ -42,21 +41,21 @@ def tar_mission(request, passed_data={}):
     data = {
       'create_success': False,
       'what_was_wrong_with_the_tarball': '',
-      'filenames_for_tarball': TarMission.FILES.keys(),
+      'filenames_for_tarball': controllers.TarMission.FILES.keys(),
       'create_form': forms.TarUploadForm(),
       'unpack_form': forms.TarExtractUploadForm(),
       'unpack_success': False,
-      'tarball_for_unpacking_mission': UntarMission.TARBALL_NAME,
-      'file_we_want': UntarMission.FILE_WE_WANT,
-      'create_done': mission_completed(request.user.get_profile(), 'tar'),
-      'unpack_done': mission_completed(request.user.get_profile(), 'tar_extract')
+      'tarball_for_unpacking_mission': controllers.UntarMission.TARBALL_NAME,
+      'file_we_want': controllers.UntarMission.FILE_WE_WANT,
+      'create_done': controllers.mission_completed(request.user.get_profile(), 'tar'),
+      'unpack_done': controllers.mission_completed(request.user.get_profile(), 'tar_extract')
     }
     data.update(passed_data)
     return (request, 'missions/tar_upload.html', data)
 
 def tar_file_download(request, name):
-    if name in TarMission.FILES:
-        response = HttpResponse(TarMission.FILES[name])
+    if name in controllers.TarMission.FILES:
+        response = HttpResponse(controllers.TarMission.FILES[name])
         # force it to be presented as a download
         response['Content-Disposition'] = 'attachment; filename=%s' % name
         response['Content-Type'] = 'application/octet-stream'
@@ -66,9 +65,9 @@ def tar_file_download(request, name):
 
 
 def tar_download_tarball_for_extract_mission(request):
-    response = HttpResponse(UntarMission.synthesize_tarball())
+    response = HttpResponse(controllers.UntarMission.synthesize_tarball())
     # force presentation as download
-    response['Content-Disposition'] = 'attachment; filename=%s' % UntarMission.TARBALL_NAME
+    response['Content-Disposition'] = 'attachment; filename=%s' % controllers.UntarMission.TARBALL_NAME
     response['Content-Type'] = 'application/octet-stream'
     return response
 
@@ -78,7 +77,7 @@ def tar_extract_mission_upload(request):
     if request.method == 'POST':
         form = forms.TarExtractUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            if form.cleaned_data['extracted_file'].read() == UntarMission.get_contents_we_want():
+            if form.cleaned_data['extracted_file'].read() == controllers.UntarMission.get_contents_we_want():
                 data['unpack_success'] = True
                 StepCompletion.objects.get_or_create(person=request.user.get_profile(), step=Step.objects.get(name='tar_extract'))
             else:
@@ -87,7 +86,9 @@ def tar_extract_mission_upload(request):
     return tar_mission(request, data)
 
 def diffpatch_patchsingle_get_original_file(request):
-    return make_download(open(PatchSingleFileMission.OLD_FILE).read(), filename=os.path.basename(PatchSingleFileMission.OLD_FILE))
+    return make_download(open(controllers.PatchSingleFileMission.OLD_FILE).read(),
+                         filename=os.path.basename(controllers.PatchSingleFileMission.OLD_FILE))
 
 def diffpatch_patchsingle_get_patch(request):
-    return make_download(PatchSingleFileMission.get_patch(), filename=PatchSingleFileMission.PATCH_FILENAME)
+    return make_download(controllers.PatchSingleFileMission.get_patch(),
+                         filename=controllers.PatchSingleFileMission.PATCH_FILENAME)
