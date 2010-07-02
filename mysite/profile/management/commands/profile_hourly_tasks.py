966c466fd31c1d07d6b978fa4b26d6d068fd8b37
Asheesh Laroia <asheesh@openhatch.org>
2010-07-01 20:00:16 -0400

    Set default logging level to WARN.

--- mysite/profile/management/commands/profile_hourly_tasks.py
+++ mysite/profile/management/commands/profile_hourly_tasks.py
@@ -22,6 +22,8 @@ class Command(BaseCommand):
     help = "Run this once hourly for the OpenHatch profile app."
 
     def handle(self, *args, **options):
+        rootLogger = logging.getLogger('')
+        rootLogger.setLevel(logging.WARN)
         mysite.profile.tasks.sync_bug_epoch_from_model_then_fill_recommended_bugs_cache()
         mysite.profile.tasks.fill_recommended_bugs_cache()
 
