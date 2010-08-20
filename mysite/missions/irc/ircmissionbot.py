8f5121f21e81e1ed18dc1ead8340da2f80c72bba
John Stumpo <stump@jstump.com>
2010-08-20 15:24:29 -0400

    Implement saying hello to the bot.

--- mysite/missions/irc/ircmissionbot.py
+++ mysite/missions/irc/ircmissionbot.py
@@ -5,6 +5,8 @@ from mysite.missions.base import controllers
 from ircbot import SingleServerIRCBot
 
 class IrcMissionBot(SingleServerIRCBot):
+    # States in which a session can be
+    STATE_SAID_HI = 1
 
     def __init__(self):
         SingleServerIRCBot.__init__(self, [settings.IRC_MISSION_SERVER],
@@ -57,3 +59,29 @@ class IrcMissionBot(SingleServerIRCBot):
         channel = event.target()
         if channel == self.channel:
             self.destroy_session(nick)
+
+    def on_privmsg(self, conn, event):
+        nick = event.source().split('!')[0]
+        target = event.target()
+        msg = event.arguments()[0]
+        if target == conn.get_nickname():
+            self.handle_private_message(nick, msg, conn)
+        elif target == self.channel:
+            self.handle_channel_message(nick, msg, conn)
+    on_pubmsg = on_privmsg
+
+    def handle_private_message(self, nick, msg, conn):
+        pass
+
+    def handle_channel_message(self, nick, msg, conn):
+        if nick not in self.active_sessions:
+            # Check for the hello message.
+            mynick_lower = conn.get_nickname().lower()
+            msg_lower = msg.lower()
+            if mynick_lower in msg_lower and ('hello' in msg_lower or 'hi' in msg_lower):
+                try:
+                    session = IrcMissionSession.objects.get(nick=nick, person__isnull=False)
+                    self.active_sessions[nick] = self.STATE_SAID_HI
+                    conn.privmsg(self.channel, "Hi! Nice to have another person here.  We're busy but we try to be friendly, so be sure to check the channel topic.")
+                except IrcMissionSession.DoesNotExist:
+                    conn.privmsg(self.channel, "Good to see you, %s.  Be sure you check the private message I sent you so you can start the mission." % nick)
