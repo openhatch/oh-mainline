a49cf316898cc0888164bec275a3f70b984132a4
Asheesh Laroia <asheesh@asheesh.org>
2010-09-27 18:14:18 -0400

    Create new management command for getting user email addresses
    This helps me write "Sorry things broke" emails.

---
+++ mysite/profile/management/commands/email_address_from_session_id.py
@@ -0,0 +1,16 @@
+import datetime
+import logging
+import sys
+
+from django.core.management.base import BaseCommand
+
+import mysite.profile.models
+
+class Command(BaseCommand):
+    help = "Reads a session ID on stdin, and prints an email address for the person."
+
+    def handle(self, *args, **options):
+        for session_id in sys.stdin:
+            p = Person.get_from_session_key(session_id)
+            print repr(p.get_full_name_or_username()),
+            print '<' + p.user.email + '>'
