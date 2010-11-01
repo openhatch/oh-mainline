53e1e562f32b62e5395086c675bb517364467f56
Asheesh Laroia <asheesh@asheesh.org>
2010-11-01 11:55:41 -0400

    Add wrapper script that uploads snapshot.

---
+++ mysite/scripts/snapshot_then_push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## change directory into the right place
+PREFIX="."
+while [ ! -d "$PREFIX/mysite" ]; do
+    PREFIX="../$PREFIX"
+done
+
+cd "$PREFIX"
+
+## Create a path to store the JSON in
+TEMPFILE="$(mktemp --suffix -snapshot.json.gz)"
+
+## Do the slooow thing of snapshotting the database
+./bin/production snapshot_public_data | gzip > "$TEMPFILE"
+
+## push this somewhere so that we save it
+rsync -q "$TEMPFILE" inside@inside.openhatch.org:/var/web/inside.openhatch.org/snapshots/$(date -I).json.gz
