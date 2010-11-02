6df5598b15bfcb4987c16197ff247adf0ba405e2
Asheesh Laroia <asheesh@asheesh.org>
2010-11-02 17:26:32 -0400

    Permit people to download the thing.

--- mysite/scripts/snapshot_then_push.sh
+++ mysite/scripts/snapshot_then_push.sh
@@ -14,6 +14,10 @@ TEMPFILE="$(mktemp --suffix -snapshot.json.gz)"
 ## Do the slooow thing of snapshotting the database
 ./bin/production snapshot_public_data | gzip > "$TEMPFILE"
 
+# Set the permissions so that, after the file gets pushed to the web,
+# the web server permits people to download these snapshots.
+chmod 644 "$TEMPFILE"
+
 ## push this somewhere so that we save it
 rsync -q "$TEMPFILE" inside@inside.openhatch.org:/var/web/inside.openhatch.org/snapshots/$(date -I).json.gz
 
