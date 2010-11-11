8d306005b7bb0e6ad41b599fdf29e0dd22ce5838
Asheesh Laroia <asheesh@openhatch.org>
2010-11-11 11:44:12 -0500

    database creation: Properly set up UTF-8 database.

--- mysite/scripts/database_01_create.sql
+++ mysite/scripts/database_01_create.sql
@@ -1,2 +1,3 @@
-SET NAMES utf8;                                                     -- set the default character set to work with Unicode
-CREATE DATABASE oh_milestone_a;                                     -- so there is somewhere to store the data
+CREATE DATABASE oh_milestone_a DEFAULT CHARACTER SET = utf8;          -- so there is somewhere to store the data
+GRANT ALL ON oh_milestone_a.* TO 'oh_milestone_a'@'localhost' IDENTIFIED BY 'ahmaC0Th'; -- so that ./bin/mysite has a database
+GRANT ALL ON test_oh_milestone_a.* TO 'oh_milestone_a'@'localhost'; -- for the test suite
