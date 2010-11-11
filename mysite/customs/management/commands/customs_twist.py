7046a585066b44379ee2a5978bcaefff00dffbc1
Asheesh Laroia <asheesh@openhatch.org>
2010-11-11 11:25:16 -0500

    customs_twist: If there is no work to do, exit.

--- mysite/customs/management/commands/customs_twist.py
+++ mysite/customs/management/commands/customs_twist.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 import twisted.web.client
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
 import mysite.customs.profile_importers
 
 import mysite.profile.models
@@ -10,10 +10,13 @@ class Command(BaseCommand):
 
     def create_tasks_from_dias(self):
         print 'For all DIAs we know how to process with Twisted: enqueue them.'
+        deferreds_we_created = []
         for dia in mysite.profile.models.DataImportAttempt.objects.filter(completed=False):
             if dia.source in mysite.customs.profile_importers.SOURCE_TO_CLASS:
                 cls = mysite.customs.profile_importers.SOURCE_TO_CLASS[dia.source]
-                self.add_dia_to_reactor(cls, dia.query, dia.id)
+                d = self.add_dia_to_reactor(cls, dia.query, dia.id)
+                deferreds_we_created.append(d)
+        return deferreds_we_created
 
     def add_dia_to_reactor(self, cls, query, dia_id):
         ### For now, only the 'db' == Debian == qa.debian.org DIAs are in a format where
@@ -26,9 +29,20 @@ class Command(BaseCommand):
         d = twisted.web.client.getPage(url)
         d.addCallback(state_manager.handlePageContents)
         d.addErrback(state_manager.handleError)
+        return d
+
+    def enqueue_reactor_death(self, deferreds):
+        dl = defer.DeferredList(deferreds)
+        dl.addCallback(lambda x: reactor.stop())
 
     def handle(self, *args, **options):
-        self.create_tasks_from_dias()
-        print 'Starting Reactor...'
-        reactor.run()
-        print '...reactor finished!'
+        print "Creating getPage()-based deferreds..."
+        created_deferreds = self.create_tasks_from_dias()
+        if created_deferreds:
+            print "Creating a DeferredList that, when all those are done, will quit the reactor..."
+            self.enqueue_reactor_death(created_deferreds)
+            print 'Starting Reactor...'
+            reactor.run()
+            print '...reactor finished!'
+        else:
+            print "We have no work to do. Exiting."
