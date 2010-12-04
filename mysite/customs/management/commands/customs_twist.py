66385f566ca15385fff4f023a4f87694fb2e807e
Asheesh Laroia <asheesh@openhatch.org>
2010-12-04 13:13:49 -0500

    customs_twist: Stop using a DeferredList to track the getPage requests.
    This way, as we add Deferreds at runtime, we can still stop the reactor as needed.

--- mysite/customs/management/commands/customs_twist.py
+++ mysite/customs/management/commands/customs_twist.py
@@ -11,16 +11,12 @@ class Command(BaseCommand):
 
     def create_tasks_from_dias(self):
         print 'For all DIAs we know how to process with Twisted: enqueue them.'
-        deferreds_we_created = []
         for dia in mysite.profile.models.DataImportAttempt.objects.filter(completed=False):
             if dia.source in mysite.customs.profile_importers.SOURCE_TO_CLASS:
                 cls = mysite.customs.profile_importers.SOURCE_TO_CLASS[dia.source]
-                created = self.add_dia_to_reactor(cls, dia.query, dia.id)
-                deferreds_we_created.extend(created)
-        return deferreds_we_created
+                self.add_dia_to_reactor(cls, dia.query, dia.id)
 
     def add_dia_to_reactor(self, cls, query, dia_id):
-        deferreds_created = []
         ### For now, only the 'db' == Debian == qa.debian.org DIAs are in a format where
         ### they can handle asynchronous operation.
         state_manager = cls(query, dia_id)
@@ -31,13 +27,14 @@ class Command(BaseCommand):
         for data_dict in urls_and_callbacks:
             logging.debug("Creating getPage for " + data_dict['url'] + 'due to DIA with id ' + str(dia_id))
             d = state_manager.createDeferredAndKeepTrackOfIt(data_dict['url'])
-
+            self.running_deferreds += 1
             # wrap the callback
             wrapped_callback = mysite.customs.profile_importers.ImportActionWrapper(
                 url=data_dict['url'],
                 pi=state_manager,
                 fn=data_dict['callback'])
             d.addCallback(wrapped_callback)
+            d.addCallback(self.decrement_deferred_count_and_maybe_quit)
 
             if 'errback' in data_dict:
                 # wrap the errback
@@ -47,21 +44,21 @@ class Command(BaseCommand):
                     fn=data_dict['errback'])
                 d.addErrback(wrapped_errback)
 
-            deferreds_created.append(d)
-        return deferreds_created
+    def decrement_deferred_count_and_maybe_quit(self, *args):
+        self.running_deferreds -= 1
+        if self.running_deferreds == 0:
+            self.stop_the_reactor()
+        if self.running_deferreds < 0:
+            raise ValueError("Uh, number of running deferreds went negative.")
 
     def stop_the_reactor(self, *args):
         reactor.callWhenRunning(lambda: reactor.stop())
 
-    def enqueue_reactor_death(self, deferreds):
-        dl = defer.DeferredList(deferreds)
-        dl.addCallback(self.stop_the_reactor)
-
     def handle(self, *args, **options):
+        self.running_deferreds = 0
+
         print "Creating getPage()-based deferreds..."
-        created_deferreds = self.create_tasks_from_dias()
-        print "Creating a DeferredList that, when all those are done, will quit the reactor..."
-        self.enqueue_reactor_death(created_deferreds)
+        self.create_tasks_from_dias()
         print 'Starting Reactor...'
         reactor.run()
         print '...reactor finished!'
