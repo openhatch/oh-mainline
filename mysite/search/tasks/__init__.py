e118135aa4e78216083833786ad7cc7d7686f764
Asheesh Laroia <asheesh@openhatch.org>
2011-01-23 15:14:01 -0500

    Mister Deployed is not a person, so it cannot hold copyright
    That is the name that Asheesh sometimes uses, when he makes a commit while logged
    in to the deployment.

--- mysite/search/tasks/__init__.py
+++ mysite/search/tasks/__init__.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2010 Parker Phinney
 # Copyright (C) 2010 Jack Grigg
 # Copyright (C) 2009, 2010 OpenHatch, Inc.
-# Copyright (C) 2010 Mister Deployed
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
