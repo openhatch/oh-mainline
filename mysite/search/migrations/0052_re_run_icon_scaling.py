e50d226f2df447d0a9d262ddc8d06a2cdf278cb3
Asheesh Laroia <asheesh@asheesh.org>
2011-02-02 18:27:48 -0500

    Removed broken migration

--- mysite/search/migrations/0052_re_run_icon_scaling.py
+++ mysite/search/migrations/0052_re_run_icon_scaling.py
@@ -27,9 +27,9 @@ class Migration:
     def forwards(self, orm):
         "Write your forwards migration here"
         # OH MY GOD THIS is theoretically lame. But I don't care, so there.
-        for project in Project.objects.all():
-            print 'rescaling', project.name
-            project.update_scaled_icons_from_self_icon()
+        #for project in Project.objects.all():
+        #    print 'rescaling', project.name
+        #    project.update_scaled_icons_from_self_icon()
     
     
     def backwards(self, orm):
