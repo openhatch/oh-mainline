4222b795258f18554d042e9b0e8c4dc71db89467
Asheesh Laroia <asheesh@asheesh.org>
2011-02-23 08:38:09 -0500

    We no longer use WebResponse

--- mysite/customs/tests.py
+++ mysite/customs/tests.py
@@ -1207,44 +1207,6 @@ class UserGetsMessagesDuringImport(django.test.TestCase):
 
         self.assertEqual(len(paulproteus.user.get_and_delete_messages()), 1)
 
-class OhlohLogging(django.test.TestCase):
-    fixtures = ['user-paulproteus', 'person-paulproteus']
-
-    @mock.patch('mysite.profile.tasks.FetchPersonDataFromOhloh', MockFetchPersonDataFromOhloh)
-    def test_we_save_ohloh_data_in_success(self):
-        # Create a DIA
-        # Ask it to do_what_it_says_on_the_tin
-        # That will cause it to go out to the network and download some data from Ohloh.
-        # Two cases to verify:
-        # 1. An error - verify that we save the HTTP response code
-        paulproteus = Person.objects.get(user__username='paulproteus')
-        success_dia = mysite.profile.models.DataImportAttempt(
-            source='rs', person=paulproteus, query='queree')
-        success_dia.save()
-        success_dia.do_what_it_says_on_the_tin() # go out to Ohloh
-
-        # refresh the DIA with the data from the database
-        success_dia = mysite.profile.models.DataImportAttempt.objects.get(
-            pk=success_dia.pk)
-        self.assertEqual(success_dia.web_response.status, 200)
-        self.assert_('ohloh.net' in success_dia.web_response.url)
-        self.assert_('<?xml' in success_dia.web_response.text)
-
-    @mock.patch('mechanize.Browser.open', None)
-    def _test_we_save_ohloh_data_in_failure(self):
-        # Create a DIA
-        # Ask it to do_what_it_says_on_the_tin
-        # That will cause it to go out to the network and download some data from Ohloh.
-        # Two cases to verify:
-        # 2. Success - verify that we store the same data Ohloh gave us back
-        paulproteus = Person.objects.get(user__username='paulproteus')
-        success_dia = DataImportAttempt(
-            source='rs', person=paulproteus, query='queree')
-        success_dia.do_what_it_says_on_the_tin() # go out to Ohloh
-        self.assertEqual(error_dia.web_response.text, 'response text')
-        self.assertEqual(error_dia.web_response.url, 'http://theurl.com/')
-        self.assertEqual(error_dia.web_response.status, 200)
-
 class MercurialRoundupGrab(django.test.TestCase):
 
     closed_bug_filename = os.path.join(settings.MEDIA_ROOT, 'sample-data',
