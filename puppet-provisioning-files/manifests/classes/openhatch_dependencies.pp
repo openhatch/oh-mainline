e7aa2c7bb1e897558ca0045162def0f8429fd4d0
Asheesh Laroia <asheesh@asheesh.org>
2011-07-04 15:14:42 -0400

    Improving the Puppets scripts

---
+++ puppet-provisioning-files/manifests/classes/openhatch_dependencies.pp
@@ -0,0 +1,11 @@
+# This Puppet class installs the OpenHatch codebase, its
+# package dependencies, and runs 'buildout'.
+class openhatch_dependencies {
+  package { ['python2.6-dev', 'python-libxml2', 'memcached', 'python-mysqldb',
+             'python-setuptools', 'libxml2-dev', 'libxslt-dev', 'mysql-server',
+             'mysql-client', 'python-xapian', 'python-imaging', 'subversion',
+             'git-core']:
+    ensure => installed,
+  }
+}
+
--- puppet-provisioning-files/manifests/lucid32.pp
+++ puppet-provisioning-files/manifests/lucid32.pp
@@ -4,16 +4,28 @@
 import "classes/*"
 
 class openhatch_code {
-  package { ['python2.6-dev', 'python-libxml2', 'memcached', 'python-mysqldb',
-             'python-setuptools', 'libxml2-dev', 'libxslt-dev', 'mysql-server',
-             'mysql-client', 'python-xapian', 'python-imaging', 'subversion',
-             'git-core']:
-    ensure => installed,
+  # Run bootstrap.py to make sure buildout can run
+  exec { "/usr/bin/python2.6 /vagrant/bootstrap.py":
+    creates => '/vagrant/bin/buildout',
+    user => 'vagrant',
+    group => 'vagrant',
+    cwd => '/vagrant/',
+    require => [Package['python2.6-dev']],
+  }
+
+  exec { "/vagrant/bin/buildout":
+    creates => '/vagrant/bin/mysite',
+    user => 'vagrant',
+    group => 'vagrant',
+    cwd => '/vagrant/',
+    logoutput => true,
+    require => [Exec["/usr/bin/python2.6 /vagrant/bootstrap.py"]],
   }
 
 }
 
 node default {
   include apt_get_update
+  include openhatch_dependencies
   include openhatch_code
 }
