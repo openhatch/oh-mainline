2c3c7911688cbafa3a4b1561a86befea81e08e49
Asheesh Laroia <asheesh@asheesh.org>
2011-07-05 18:17:40 -0400

    Moving openhatch_code Puppet class to its own file

--- puppet-provisioning-files/manifests/squeeze.pp
+++ puppet-provisioning-files/manifests/squeeze.pp
@@ -10,28 +10,6 @@ class {
   'apt_get_update': stage => first
 }
 
-
-class openhatch_code {
-  # Run bootstrap.py to make sure buildout can run
-  exec { "/usr/bin/python2.6 /vagrant/bootstrap.py":
-    creates => '/vagrant/bin/buildout',
-    user => 'vagrant',
-    group => 'vagrant',
-    cwd => '/vagrant/',
-  }
-
-  exec { "/vagrant/bin/buildout":
-    creates => '/vagrant/bin/mysite',
-    user => 'vagrant',
-    timeout => 0,
-    group => 'vagrant',
-    cwd => '/vagrant/',
-    logoutput => true,
-    require => [Exec["/usr/bin/python2.6 /vagrant/bootstrap.py"]],
-  }
-
-}
-
 node default {
   include apt_get_update
   include openhatch_dependencies
