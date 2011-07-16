4a9abab146eed317a84fdfd783315317c9a2d827
Asheesh Laroia <asheesh@asheesh.org>
2011-07-16 10:23:34 -0400

    Fix libxslt-dev dependency to a non-virtual package

--- puppet-provisioning-files/manifests/classes/openhatch_dependencies.pp
+++ puppet-provisioning-files/manifests/classes/openhatch_dependencies.pp
@@ -2,7 +2,7 @@
 # package dependencies, and runs 'buildout'.
 class openhatch_dependencies {
   package { ['python2.6-dev', 'python-libxml2', 'memcached', 'python-mysqldb',
-             'python-setuptools', 'libxml2-dev', 'libxslt-dev', 'mysql-server',
+             'python-setuptools', 'libxml2-dev', 'libxslt1-dev', 'mysql-server',
              'mysql-client', 'python-xapian', 'python-imaging', 'subversion',
              'git-core']:
     ensure => installed,
