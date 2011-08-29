5ae639ffc69b020306148eb739574d4756c57d61
Asheesh Laroia <asheesh@asheesh.org>
2011-08-29 11:51:25 -0400

    Make bootstrap.py use distribute by default

--- bootstrap.py
+++ bootstrap.py
@@ -32,7 +32,7 @@ parser = OptionParser()
 parser.add_option("-v", "--version", dest="version",
                           help="use a specific zc.buildout version")
 parser.add_option("-d", "--distribute",
-                   action="store_true", dest="distribute", default=False,
+                   action="store_true", dest="distribute", default=True,
                    help="Use Disribute rather than Setuptools.")
 
 parser.add_option("-c", None, action="store", dest="config_file",
