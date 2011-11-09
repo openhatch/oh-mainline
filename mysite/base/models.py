a6f490f0563ff500aeb502d23f7557836e47dd1f
Asheesh Laroia <asheesh@asheesh.org>
2011-11-09 18:20:14 -0500

    If we are using sqlite3, activate support for checking foreign keys
    Source: http://stackoverflow.com/questions/4534992/place-to-set-sqlite-pragma-option-in-django-project

--- mysite/base/models.py
+++ mysite/base/models.py
@@ -59,3 +59,12 @@ class Timestamp(models.Model):
         "model last updated " to it.'''
         s = 'model last updated ' + cls.__module__ + '.' + cls.__name__
         return s
+
+### Adjustments to default Django sqlite3 behavior
+def activate_foreign_keys(sender, connection, **kwargs):
+    """Enable integrity constraint with sqlite."""
+    if connection.vendor == 'sqlite':
+        cursor = connection.cursor()
+        cursor.execute('PRAGMA foreign_keys = ON;')
+from django.db.backends.signals import connection_created
+connection_created.connect(activate_foreign_keys)
