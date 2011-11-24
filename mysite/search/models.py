6bf6e9faacbf89ed5e5e29d82eb3e40c2637a174
Asheesh Laroia <asheesh@asheesh.org>
2011-11-24 10:04:58 -0500

    Introduce new potential_mentor_count method on Project (with a test)

--- mysite/search/models.py
+++ mysite/search/models.py
@@ -107,6 +107,19 @@ class Project(OpenHatchModel):
         return self.name
 
     @mysite.base.decorators.cached_property
+    def potential_mentor_count(self):
+        '''Return a number of potential mentors, counted as the
+        number of people who can mentor in the project by name unioned
+        with those who can mentor in the project's language.'''
+        all_mentor_person_ids = set()
+        import mysite.profile.controllers
+        for way_a_mentor_can_help in (self.name, self.language):
+            tq = mysite.profile.controllers.TagQuery('can_mentor',
+                                                     way_a_mentor_can_help)
+            all_mentor_person_ids.update(tq.people.values_list('id', flat=True))
+        return len(all_mentor_person_ids)
+
+    @mysite.base.decorators.cached_property
     def potential_mentors(self):
         """Return the union of the people who can mentor in this project,
         or who can mentor in the project's language."""
