51de1a5c8cb200fe1b2cd3cadd35dfc64632c879
Asheesh Laroia <asheesh@asheesh.org>
2011-11-30 22:47:34 -0500

    Removed Python.org Roundup instance

--- mysite/customs/bugtrackers/roundup.py
+++ mysite/customs/bugtrackers/roundup.py
@@ -231,37 +231,3 @@ class RoundupTracker(object):
 
     def __unicode__(self):
         return "<Roundup bug tracker for %s>" % self.root_url
-
-class PythonTracker(RoundupTracker):
-    enabled = True
-
-    def __init__(self):
-        RoundupTracker.__init__(self,
-                                root_url='http://bugs.python.org/',
-                                tracker_name='Python')
-
-    def extract_bug_tracker_specific_data(self, metadata_dict, bug_object):
-        bug_object.good_for_newcomers = (
-            'easy' in metadata_dict['Keywords'])
-        bug_object.concerns_just_documentation = (
-            'Documentation' in metadata_dict['Components'])
-        bug_object.status = metadata_dict['Status']
-        bug_object.looks_closed = (
-            metadata_dict['Status'] == 'closed' or
-            'patch' in metadata_dict['Keywords'])
-        bug_object.importance = metadata_dict['Priority']
-
-    def generate_list_of_bug_ids_to_look_at(self):
-        ### bug queries to look at
-        queries = {
-            'Documentation bugs':
-                'http://bugs.python.org/issue?status=1%2C3&%40sort=activity&%40columns=id&%40startwith=0&%40group=priority&%40filter=status%2Ccomponents&components=4&%40action=export_csv',
-            'Easy bugs':
-                'http://bugs.python.org/issue?status=1%2C3&%40sort=activity&%40columns=id&%40startwith=0&%40group=priority&%40filter=status%2Ckeywords&keywords=6&%40action=export_csv',
-            }
-
-        for query_name in queries:
-            query_url = queries[query_name]
-            for bug_id in csv_url2bugs(query_url):
-                yield bug_id
-
