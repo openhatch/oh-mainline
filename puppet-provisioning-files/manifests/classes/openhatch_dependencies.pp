1256385fdbbeb48fb09aff307948c317bea7200d
Asheesh Laroia <asheesh@asheesh.org>
2012-03-12 14:02:04 -0700

    Remove the final mentions of libxml2

--- puppet-provisioning-files/manifests/classes/openhatch_dependencies.pp
+++ puppet-provisioning-files/manifests/classes/openhatch_dependencies.pp
@@ -1,8 +1,8 @@
 # This Puppet class installs the OpenHatch codebase, its
 # package dependencies, and runs 'buildout'.
 class openhatch_dependencies {
-  package { ['python2.6-dev', 'python-libxml2', 'memcached', 'python-mysqldb',
-             'python-setuptools', 'libxml2-dev', 'libxslt1-dev', 'mysql-server',
+  package { ['python2.6-dev', 'memcached', 'python-mysqldb',
+             'python-setuptools', 'libxslt1-dev', 'mysql-server',
              'mysql-client', 'python-xapian', 'python-imaging', 'subversion',
              'git-core']:
     ensure => installed,
