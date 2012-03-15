1372f57a10c95c39c375f79aff42b8126d2165d9
Daniel Mizyrycki <mdaniel@glidelink.net>
2012-03-15 11:57:51 -0700

    Add Sphinx documentation to OpenHatch

---
+++ docs/generate_html.py
@@ -0,0 +1,18 @@
+"""Generate html documentation"""
+
+__requires__ = 'Sphinx==1.1.2'
+
+import sys,os,re,conf
+from pkg_resources import load_entry_point
+
+
+def main(argv=None):
+    if argv:
+        sys.argv = argv
+    # Generate documentation
+    return load_entry_point('Sphinx==1.1.2', 'console_scripts',
+        'sphinx-build')()
+
+if __name__ == "__main__":
+    sys.exit(main(['generate_html.py','-b','html','-d','_temp','.','html']))
+
---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. OpenHatch documentation master file, created by
+   sphinx-quickstart on Tue Mar 13 16:08:25 2012.
+
+Welcome to OpenHatch's documentation
+====================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting_started
+   handling_patches
+   continuous_integration
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
