bdb954b652a4085da717d89b28bed2a41275f8af
Asheesh Laroia <asheesh@asheesh.org>
2012-05-13 15:52:24 -0700

    If manage.py is run from an unusual place, chdir() so we can find mysite/manage.py
    This, on a dev setup, fixes a problem where the Subversion training
    mission's pre-commit fails, exiting with exit status code 1.

--- manage.py
+++ manage.py
@@ -3,7 +3,12 @@ import os
 import sys
 
 if not os.path.exists('mysite/manage.py'):
-    print "Eek, where is the real manage.py? Quitting."
-    sys.exit(1)
+    # Try one more thing -- chdir() into the path of this file
+    # and try to find mysite/manage.py. Note that chdir()ing is
+    # a somewhat intense strategy here.
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    if not os.path.exists('mysite/manage.py'):   
+        print "Eek, where is the real manage.py? Quitting."
+        sys.exit(1)
 
 execfile('mysite/manage.py', globals(), locals())
