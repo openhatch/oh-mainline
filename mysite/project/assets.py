bfb7e07e5cc858cadef904d9125cee43936cb0f6
Shawn Landden <shawnlandden@gmail.com>
2012-05-25 22:15:15 -0700

    move css assets to project/asset.py

--- mysite/project/assets.py
+++ mysite/project/assets.py
@@ -2,6 +2,34 @@
 from django_assets import Bundle, register
 #http://elsdoerfer.name/docs/django-assets/bundles.html
 
+register('openhatch_css',
+         Bundle("css/base/base.css",
+                "css/jquery.autocomplete.css",
+                "css/jquery-ui-lightness/jquery-ui-1.7.2.custom.css",
+                "openid/css/openid.css",
+                "css/base/jquery.jgrowl.css",
+                "css/senseknocker/thing.css",
+                "css/search/search.css",
+                "css/base/landing.css",
+                "css/base/landing_page_for_logged_in_users.css",
+                "css/profile/person_small.css",
+                "css/project/project.css",
+                "css/base/one_column.css",
+                "css/base/two_columns.css",
+                "css/base/three_columns.css",
+                "css/account/auth.css",
+                "css/account/settings.css",
+                "css/account/widget.css",
+                "css/qunit.css",
+                "css/profile/base.css",
+                "css/profile/portfolio.css",
+                "css/profile/search_people.css",
+                "css/tipsy.css",
+                "css/facebox.css",
+                "css/missions/base.css",
+                "css/jquery-ui-lightness/jquery-ui-1.7.2.complete.css",
+                ),
+         output="packed/openhatch.css")
 register('header_js',
          # jQuery
          Bundle('js/jquery-1.3.2.js',
@@ -21,9 +49,16 @@ register('openhatch_js',
                 'js/facebox.js',
                 ),
          # jQuery UI - TODO: import source, and each part individually
+         # 1.7.2: core, sortable, progressbar, tabs
          Bundle('js/jquery-ui-1.7.2.custom.min.js',
                 'js/jquery.tabs.js',
          	),
+         #Bundle('jquery-ui/ui/jquery.ui.core.js',
+         #       'jquery-ui/ui/jquery.ui.sortable.js',
+         #       'jquery-ui/ui/jquery.ui.progressbar.js',
+         #       'jquery-ui/ui/jquery.ui.tabs.js',
+         #       ),
+         
          # jQuery Tools
          Bundle('openid/js/openid-jquery.js',
                 'js/facebox.js',
