f5a2ce21f996410787b681407cca73fea50cb5af
Asheesh Laroia <asheesh@asheesh.org>
2012-05-30 15:25:16 -0700

    Adding dj-database-url

---
+++ vendor/packages/dj-database-url/dj_database_url.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import os
+import urlparse
+
+# Register database schemes in URLs.
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('mysql')
+urlparse.uses_netloc.append('sqlite')
+
+DEFAULT_ENV = 'DATABASE_URL'
+
+def config(env=DEFAULT_ENV, default=None):
+    """Returns configured DATABASE dictionary from DATABASE_URL."""
+
+    config = {}
+
+    if env in os.environ:
+        s = os.environ[env]
+    else:
+        s = default
+
+    if s:
+        config = parse(s)
+
+    return config
+
+
+def parse(url):
+    """Parses a database URL."""
+
+    config = {}
+
+    url = urlparse.urlparse(url)
+
+    # Update with environment configuration.
+    config.update({
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+    })
+
+    if url.scheme == 'postgres':
+        config['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+    if url.scheme == 'mysql':
+        config['ENGINE'] = 'django.db.backends.mysql'
+
+    if url.scheme == 'sqlite':
+        config['ENGINE'] = 'django.db.backends.sqlite3'
+
+    return config
