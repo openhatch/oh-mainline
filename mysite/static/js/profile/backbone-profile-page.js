8c0f8a73f679261adefe5a19b63427279e55ac0d
Karen Rustad <karen.rustad@gmail.com>
2012-08-25 21:51:36 -0700

    wow, backbone generates things properly now

--- mysite/static/js/profile/backbone-profile-page.js
+++ mysite/static/js/profile/backbone-profile-page.js
@@ -4,10 +4,11 @@ Profile.addRegions({
     portfolio: "#portfolio-entries"
 });
     
-PortfolioEntry = new Backbone.Model.extend({
+PortfolioEntry = Backbone.Model.extend({
     defaults: {
         project__name: 'Twisted',
         project__icon: '/404.png',
+        project_id: 1,
         project_url: '/+projects/twisted/',
         project_description: 'The engine of your internets',
         experience_description: 'I hacked the RSS feed reader.',
@@ -22,7 +23,7 @@ PortfolioEntry = new Backbone.Model.extend({
         }
 });
     
-Portfolio = new Backbone.Collection.extend({
+Portfolio = Backbone.Collection.extend({
     model: PortfolioEntry,
     comparator: function(entry){
         return entry.get('sort_order');
@@ -31,16 +32,17 @@ Portfolio = new Backbone.Collection.extend({
 
 PortfolioEntryView = Backbone.Marionette.ItemView.extend({
     template: '#portfolio-entry-template',
-    className: 'portfolio_entry'
+    className: 'portfolio_entry',
+    tagName: 'li'
 });
 
 PortfolioView = Backbone.Marionette.CompositeView.extend({
     tagName: "div",
-    id: 'portfolio-entries-list',
+    id: 'portfolio-entries-all',
     template: '#portfolio-entries-list-template',
     itemView: PortfolioEntryView,
     
-    appendHtml: function(collectionView, itemView) { collectionView.$('ul').append(itemView.el); }
+    appendHtml: function(collectionView, itemView) { collectionView.$('#portfolio-entries-list').append(itemView.el); }
     });
     
 Profile.addInitializer(function(options){
@@ -55,10 +57,23 @@ $(document).ready(function(){
         new PortfolioEntry ({ 
             project__name: 'Wet Cat', 
             project_description: 'A cat that is wet.', 
-            receive_maintainer_updates: false
+            project_id: 2,
+            receive_maintainer_updates: false,
+            citation_list: [
+               {   url: 'http://whee.com', 
+                    description: 'Bugs I have fixed'
+               },
+               {   url: 'http://things.com', 
+                    description: 'Blog post about my feature'
+               },
+               {   url: 'http://pyvideo.org', 
+                    description: 'Video of my 2012 PyCon talk about Wet Cat'
+               },
+            ]
             }),
         new PortfolioEntry ({ 
             project__name: 'Bitey', 
+            project_id: 3,
             project_description: 'A rather angry cat emulator.', 
             experience_description: 'I hacked your face.'
             }),
