d00c0b804532a60d77f2012b276b3af0721217ad
Asheesh Laroia <asheesh@asheesh.org>
2012-10-26 13:49:58 -0400

    Big changes to run_importer.sh
    * Use temporary files allocated by mktemp
    * Disable telnet console and web service console
    * Let argv[1] determine the number of trackers to import

--- run_importer.sh
+++ run_importer.sh
@@ -1,7 +1,16 @@
-wget https://openhatch.org/+api/v1/customs/tracker_model/\?format\=yaml\&limit\=500 -O /tmp/bug-trackers.yaml
+#!/bin/bash
+BUG_TRACKER_LIST="$(mktemp --suffix=.yaml /tmp/bug-trackers.$(date -I).XXXX)"
+SCRAPY_RESULT_FILE="$(mktemp --suffix=.jsonlines /tmp/scrapy-results.XXXX)"
+SCRAPY_LOG="$(mktemp --suffix=.log /tmp/scrapy.XXXX)"
+MAX_TRACKERS="500"
+if [ ! -z "$1" ] ; then
+    MAX_TRACKERS="$1"
+fi
+
+wget https://openhatch.org/+api/v1/customs/tracker_model/\?format\=yaml\&limit\="$MAX_TRACKERS" -O "$BUG_TRACKER_LIST"
 
 pushd ../oh-bugimporters
-env/bin/scrapy runspider bugimporters/main.py  -a input_filename=/tmp/bug-trackers.yaml  -s FEED_FORMAT=jsonlines -s FEED_URI=/tmp/results.jsonlines  -s LOG_FILE=/tmp/scrapy-log -s CONCURRENT_REQUESTS_PER_DOMAIN=1 -s CONCURRENT_REQUESTS=200 -s DEPTH_PRIORITY=1 -s SCHEDULER_DISK_QUEUE=scrapy.squeue.PickleFifoDiskQueue -s SCHEDULER_MEMORY_QUEUE=scrapy.squeue.FifoMemoryQueue
+env/bin/scrapy runspider bugimporters/main.py  -a input_filename="$BUG_TRACKER_LIST" -s TELNETCONSOLE_ENABLED=False -s WEBSERVICE_ENABLED=False -s FEED_FORMAT=jsonlines -s FEED_URI="$SCRAPY_RESULT_FILE"  -s LOG_FILE="$SCRAPY_LOG" -s CONCURRENT_REQUESTS_PER_DOMAIN=1 -s CONCURRENT_REQUESTS=200 -s DEPTH_PRIORITY=1 -s SCHEDULER_DISK_QUEUE=scrapy.squeue.PickleFifoDiskQueue -s SCHEDULER_MEMORY_QUEUE=scrapy.squeue.FifoMemoryQueue
 popd
 
-python manage.py import_bugimporter_data /tmp/results.jsonlines
+python manage.py import_bugimporter_data "$SCRAPY_RESULT_FILE"
