e8b55bc70120fbc11f6b75ffe8051486f57d2a8b
Asheesh Laroia <asheesh@asheesh.org>
2012-10-26 14:24:38 -0400

    Provide a query string parameter that gets only stale bug tracker objects

--- mysite/customs/api.py
+++ mysite/customs/api.py
@@ -1,6 +1,9 @@
 import mysite.customs.models
 import tastypie.resources
 import tastypie.authorization
+import mysite.search.models
+import django.db.models
+import datetime
 
 
 class TrackerModelResource(tastypie.resources.ModelResource):
@@ -14,6 +17,24 @@ class TrackerModelResource(tastypie.resources.ModelResource):
         detail_allowed_methods = ['get']
         resource_name = 'tracker_model'
         authorization = tastypie.authorization.DjangoAuthorization()
+
+    def get_object_list(self, request):
+        all_objects = super(TrackerModelResource, self).get_object_list(request)
+        skip_these_ids = []
+        if request.GET.get('just_stale', '').lower() == 'yes':
+            # Note: This code is of low quality.
+            for obj in all_objects:
+                relevant_bugs = mysite.search.models.Bug.all_bugs.filter(
+                    tracker_id=obj.id)
+                # Find the minimum last_polled value
+                oldest_last_polled_data = relevant_bugs.aggregate(
+                    django.db.models.Min('last_polled'))
+                oldest_last_polled = oldest_last_polled_data['last_polled__min']
+                if oldest_last_polled >= (datetime.datetime.utcnow() -
+                                          datetime.timedelta(days=1)):
+                    skip_these_ids.append(obj.id)
+        filtered = all_objects.exclude(id__in=skip_these_ids)
+        return filtered
 
     def dehydrate(self, bundle):
         '''Actually do the work of creating the dict here.'''
