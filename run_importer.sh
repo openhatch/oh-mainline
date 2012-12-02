adb2025f04dfc9c653581a3f30994fa7f59418cd
Asheesh Laroia <asheesh@asheesh.org>
2012-12-01 19:27:47 -0500

    Let bugimporters/main.py define the Scrapy settings

--- run_importer.sh
+++ run_importer.sh
@@ -18,7 +18,7 @@ fi
 wget https://openhatch.org/+api/v1/customs/tracker_model/\?just_stale\=yes\&format\=yaml\&limit\="$MAX_TRACKERS"\&tracker_id="$TRACKER_ID" -O "$BUG_TRACKER_LIST"
 
 pushd ../oh-bugimporters
-env/bin/scrapy runspider bugimporters/main.py  -a input_filename="$BUG_TRACKER_LIST" -s TELNETCONSOLE_ENABLED=0 -s WEBSERVICE_ENABLED=0 -s FEED_FORMAT=jsonlines -s FEED_URI="$SCRAPY_RESULT_FILE"  -s LOG_FILE="$SCRAPY_LOG" -s CONCURRENT_REQUESTS_PER_DOMAIN=1 -s CONCURRENT_REQUESTS=200 -s DEPTH_PRIORITY=1 -s SCHEDULER_DISK_QUEUE=scrapy.squeue.PickleFifoDiskQueue -s SCHEDULER_MEMORY_QUEUE=scrapy.squeue.FifoMemoryQueue
+bin/python bugimporters/main.py -i "$BUG_TRACKER_LIST" -o "$SCRAPY_RESULT_FILE"
 popd
 
 python manage.py import_bugimporter_data "$SCRAPY_RESULT_FILE"
