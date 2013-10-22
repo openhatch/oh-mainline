71350c3d948f5e907867c561c63f58c1be5174e4
Pawel Reise <preise@soldevelo.com>
2013-10-22 14:35:41 +0200

    Corrected settings for demo server

--- mysite/settings_demo.py
+++ mysite/settings_demo.py
@@ -1,5 +1,6 @@
 from settings import *
 
-FORCE_SCRIPT_NAME = '/demo'
-MEDIA_URL = '/demo/static/'
-STATIC_DOC_ROOT = 'demo/static/'
+FORCE_SCRIPT_NAME = 'demo'
+MEDIA_URL = '/' + FORCE_SCRIPT_NAME + '/static/'
+STATIC_DOC_ROOT = FORCE_SCRIPT_NAME + '/static/'
+LOGIN_URL = '/' + FORCE_SCRIPT_NAME + '/account/login/'
