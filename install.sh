2fab0d393f9ac88891393af6129e74bdb84e4564
Krzysztof Kaczmarczyk <kkaczmarczyk@soldevelo.com>
2013-11-08 11:56:38 +0100

    Fixed installation script

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
+sudo pip install xmlbuilder
+sudo pip install django-crontab
+sudo pip install django-file-resubmit
+sudo pip install django-cors-headers
 rm -vf mysite/site.db
 python manage.py syncdb --noinput
 python manage.py migrate
 cat ./mysite/scripts/sql/questions_answers.sql | sqlite3 ./mysite/site.db
 cat ./mysite/scripts/sql/add_admin.sql | sqlite3 ./mysite/site.db
 cat ./mysite/scripts/sql/delete_from_zoho_trigger.sql | sqlite3 ./mysite/site.db
-sudo pip install xmlbuilder
-sudo pip install django-crontab
-sudo pip install django-file-resubmit
-sudo pip install django-cors-headers
+
 if [[ `crontab -l` == *manage.py* ]]
 then
     crontab -r
