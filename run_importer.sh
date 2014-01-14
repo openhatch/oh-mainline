117c851ba4d324a230fd6089e3ae3229b8dab564
John Morrissey <jwm@horde.net>
2014-01-13 20:42:55 -0500

    '-O-' option is cruft from old wget invocations

--- run_importer.sh
+++ run_importer.sh
@@ -28,7 +28,7 @@ fi
 URL=https://openhatch.org/+api/v1/customs/tracker_model/\?just_stale\=yes\&format\=yaml\&limit\="$MAX_TRACKERS"\&tracker_id="$TRACKER_ID"
 
 # It's OK if curl has to try 4 times.
-(curl "$URL" || curl "$URL" || curl "$URL" || curl "$URL" -O-) > "$BUG_TRACKER_LIST"
+(curl "$URL" || curl "$URL" || curl "$URL" || curl "$URL") > "$BUG_TRACKER_LIST"
 
 pushd ../oh-bugimporters
 env/bin/python bugimporters/main.py -i "$BUG_TRACKER_LIST" -o "$SCRAPY_RESULT_FILE"
