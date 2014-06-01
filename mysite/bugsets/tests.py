3276d2fbe438e4adb0877c2c460f0989702fbad0
Elana Hashman <elana@hashman.ca>
2014-06-01 16:42:13 -0400

    Add basic BugSet model and list the objects

--- mysite/bugsets/tests.py
+++ mysite/bugsets/tests.py
@@ -1,16 +1,39 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+# vim: set ai et ts=4 sw=4:
 
-Replace this with more appropriate tests for your application.
-"""
+# This file is part of OpenHatch.
+# Copyright (C) 2014 Elana Hashman
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.test import TestCase
 
+# Imports {{{
+import mysite.bugsets.views
+import mysite.bugsets.models
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from mysite.base.tests import TwillTests
+
+from django.core.urlresolvers import reverse
+# }}}
+
+class BasicBugsetListTests(TwillTests):
+    def test_bugset_names_load(self):
+        mysite.bugsets.models.BugSet.objects.create(name="best event")
+        mysite.bugsets.models.BugSet.objects.create(name="bestest event")
+        url = reverse(mysite.bugsets.views.list_index)
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, "best event")
+        self.assertContains(response, "bestest event")
--- mysite/bugsets/views.py
+++ mysite/bugsets/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from mysite.bugsets.models import BugSet
 
 def list_index(request):
-    context = {}
+    bugsets = BugSet.objects.all()
+    context = {
+        'bugsets': bugsets,
+    }
     return render(request, 'list_index.html', context)
