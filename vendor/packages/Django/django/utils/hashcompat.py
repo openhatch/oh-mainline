b61230e36e4afda548bda5d58830ff0f29e1e2db
Susan Tan <susan.tan.fleckerl@gmail.com>
2014-06-26 13:01:56 -0700

    Resolve django deprecation problems

--- vendor/packages/Django/django/utils/hashcompat.py
+++ vendor/packages/Django/django/utils/hashcompat.py
@@ -4,10 +4,6 @@ hashlib module containing both hash algorithms. Here, we provide a common
 interface to the md5 and sha constructors, depending on system version.
 """
 
-import warnings
-warnings.warn("django.utils.hashcompat is deprecated; use hashlib instead",
-              DeprecationWarning)
-
 import hashlib
 md5_constructor = hashlib.md5
 md5_hmac = md5_constructor
