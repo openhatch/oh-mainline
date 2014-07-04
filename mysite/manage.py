b1433819edfffd4344a4b3a7e14348efd3196ba2
Susan Tan <susan.tan.fleckerl@gmail.com>
2014-07-03 20:49:22 -0700

    Edit manage.py for Django1.5

--- mysite/manage.py
+++ mysite/manage.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
+import os, sys
 
-# We must chdir() up one level.
-import os
-import os.path
-import sys
 THIS_FILE = os.path.abspath(__file__)
 THIS_DIR = os.path.dirname(THIS_FILE)
 UP_ONE_DIR = os.path.join(THIS_DIR, '..')
@@ -13,13 +10,8 @@ sys.path.append(UP_ONE_DIR)
 import vendor
 vendor.vendorify()
 
-# Now we can import from third-party libraries.
-from django.core.management import execute_manager, setup_environ
-
-import mysite.settings
-
-# The first thing execute_manager does is call `setup_environ`.
-setup_environ(mysite.settings)
-
 if __name__ == "__main__":
-    execute_manager(mysite.settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
