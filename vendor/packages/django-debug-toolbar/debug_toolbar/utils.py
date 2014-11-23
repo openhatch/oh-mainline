openhatch/oh-mainline

commit e641408564fb9dde26083d510fb0a9ac591a1251

Adjust Django debug toolbar to do non-ASCII-safe string operations
We should upstream this as soon as we can.

vendor/packages/django-debug-toolbar/debug_toolbar/utils.py
@@ -32,7 +32,19 @@ def get_module_path(module_name):
             'Error importing HIDE_IN_STACKTRACES: %s' % (e,))
     else:
         source_path = inspect.getsourcefile(module)
-        if source_path.endswith('__init__.py'):
+        # Note that source_path is a byte string in the default
+        # encoding for the filesystem, which meands that
+        # calling .endswith() will attempt to convert it to
+        # ASCII.
+        #
+        # os.path.dirname() is OK dealing with byte strings
+        # in unknown encodings, but .endswith() is not, so
+        # here we cast it down to ASCII for the __init__.py
+        # check, since all we care about is if we need to
+        # call os.path.dirname() on it.
+        smashed_to_ascii = source_path.decode('ascii', 'replace').encode(
+                                              'ascii', 'replace')
+        if smashed_to_ascii.endswith('__init__.py'):
             source_path = os.path.dirname(source_path)
         return os.path.realpath(source_path)
 

commit fadd20e9fab95739d8460c79c46ff5e41506345e

Fix typo in vendor/django-debug-toolbar
Typo was introduced in
https://github.com/openhatch/oh-mainline/commit/e641408564fb9dde26083d510fb0a9ac591a1251
by Asheesh L.

vendor/packages/django-debug-toolbar/debug_toolbar/utils.py
@@ -33,7 +33,7 @@ def get_module_path(module_name):
     else:
         source_path = inspect.getsourcefile(module)
         # Note that source_path is a byte string in the default
-        # encoding for the filesystem, which meands that
+        # encoding for the filesystem, which means that
         # calling .endswith() will attempt to convert it to
         # ASCII.
         #