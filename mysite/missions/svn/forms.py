3b707f8428281e4375b2c2f0e210359bd14b16b4
Carol Willing <carolcode@willingconsulting.com>
2015-08-16 11:06:05 -0700

    Refactor comments and variables

--- mysite/missions/svn/forms.py
+++ mysite/missions/svn/forms.py
@@ -17,26 +17,27 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-import patch
+import patch # TODO verify import
 
-from django.forms import Form, ValidationError
+from django import forms
 
 from mysite.missions.base.view_helpers import get_mission_data_path
 from mysite.missions.svn import view_helpers
 
 
-class CheckoutForm(Form):
+class CheckoutForm(forms.Form):
     """
-    Mission form for checkout step to see if a user corrrectly checked out
-    the svn repo
+    A form for the svn mission's 'checkout repo' step to see if the user
+    correctly checked out the svn repo and its files
     """
-    secret_word = django.forms.CharField(
-        error_messages={'required': 'No secret word was given.'}
-    )
     SECRET_WORD_FILE = 'word.txt'
 
+    secret_word = forms.CharField(
+        error_messages={'required': 'No secret word was given.'}
+    )
+
     def __init__(self, username=None, *args, **kwargs):
-        """ Initalize checkout form and set username """
+        """ Initialize checkout form and set username """
         super(CheckoutForm, self).__init__(*args, **kwargs)
         self.username = username
 
@@ -47,65 +48,71 @@ class CheckoutForm(Form):
         """
         secret_word_trunk = view_helpers.SvnRepository(self.username).cat('/trunk/' + self.SECRET_WORD_FILE).strip()
         if self.cleaned_data['secret_word'] != secret_word_trunk:
-            raise ValidationError, 'The secret word is incorrect.'
+            raise forms.ValidationError('The secret word is incorrect.')
 
 
-class DiffForm(Form):
+class DiffForm(forms.Form):
     """
-    Mission step's form to see if a user's svn diff is correct
+    A form for the svn mission's 'diff' step to see if the user creates the
+    correct `diff` output
     """
-    diff = django.forms.CharField(
-        error_messages={'required': 'No svn diff output was given.'},
-        widget=django.forms.Textarea()
-    )
     FILE_TO_BE_PATCHED = 'README'
     NEW_CONTENT = os.path.join(get_mission_data_path('svn'),
                                'README-new-for-svn-diff')
 
-    def __init__(self, username=None, wcdir=None, request=None,
-                 *args, **kwargs):
+    diff = forms.CharField(
+        error_messages={'required': 'No svn diff output was given.'},
+        widget=forms.Textarea()
+    )
+
+    def __init__(self, username=None, working_directory=None, request=None, *args, **kwargs):
+        """ Initialize diff form """
         super(DiffForm, self).__init__(request, *args, **kwargs)
 
         self.username = username
-        self.wcdir = wcdir
-        if wcdir:
-            self.file_to_patch = os.path.join(wcdir, self.FILE_TO_BE_PATCHED)
-        self.the_patch = None
-        self.new_content = None
+        self.working_directory = working_directory
+        if working_directory:
+            self.file_to_patch = os.path.join(working_directory,
+                                              self.FILE_TO_BE_PATCHED)
+
+        # Initialize proposed_patch and proposed_content
+        self.proposed_patch = None
+        self.proposed_content = None
 
     def clean_diff(self):
         """
-        Validate the diff form.
+        Clean and validate the proposed patch contents of the `diff` form.
         This function will be invoked by django.form.Forms.is_valid(), and
         will raise the exception ValidationError
         """
-        self.the_patch = patch.fromstring(self.cleaned_data['diff'])
-        # Check that the submitted diff patches the correct number of files
-        if len(self.the_patch.hunks) != 1:
-            raise ValidationError, 'The patch affects more than one file.'
+        self.proposed_patch = patch.fromstring(self.cleaned_data['diff'])
 
-        # Check that the filename it patches is correct.
+        # Check that proposed patch affects the correct number of files
+        if len(self.proposed_patch.hunks) != 1:
+            raise forms.ValidationError('The patch affects more than one file.')
+
+        # Check that proposed patch has the correct filename.
         if self.FILE_TO_BE_PATCHED not in self.cleaned_data['diff']:
-            raise ValidationError, 'The patch affects the wrong file.'
+            raise forms.ValidationError('The patch affects the wrong file.')
 
-        # Now we need to generate a working copy to apply the patch to.
-        # We can also use this working copy to commit the patch if it's OK.
+        # Get a mission user's working copy of the svn repo
         repo = view_helpers.SvnRepository(self.username)
-        view_helpers.subproc_check_output(
-            ['svn', 'co', repo.file_trunk_url(), self.wcdir])
+        svn_checkout_command = ['svn', 'co', repo.file_trunk_url(), self.working_directory]
+        view_helpers.subproc_check_output(svn_checkout_command)
 
-        # Check that it will apply correctly to the working copy.
-        if not self.the_patch._match_file_hunks(self.file_to_patch, self.the_patch.hunks[0]):
-            raise ValidationError, 'The patch will not apply correctly to the lastest revision.'
+        # Check if proposed patch will apply correctly to the working copy.
+        if not self.proposed_patch._match_file_hunks(self.file_to_patch, self.proposed_patch.hunks[0]):
+            raise forms.ValidationError('The patch will not apply correctly to the latest revision.')
 
         # Check that the resulting file matches what is expected.
-        self.new_content = ''.join(
-            self.the_patch.patch_stream(open(self.file_to_patch), self.the_patch.hunks[0]))
-        if self.new_content != open(self.NEW_CONTENT).read():
-            raise ValidationError, 'The file resulting from patching does not have the correct contents.'
+        self.proposed_content = ''.join(self.the_patch.patch_stream(open(self.file_to_patch), self.the_patch.hunks[0]))
+        if self.proposed_content != open(self.NEW_CONTENT).read():
+            raise forms.ValidationError('The file resulting from patching does not have the correct contents.')
 
     def commit_diff(self):
-        """ Commit the diff form and the patch. """
-        open(self.file_to_patch, 'w').write(self.new_content)
-        commit_message = '''Fix a typo in %s. Thanks for reporting this, %s!''' % (self.FILE_TO_BE_PATCHED, self.username)
-        view_helpers.subproc_check_output(['svn', 'commit', '-m', commit_message, '--username', 'mr_bad', self.wcdir])
+        """ Commit the proposed patch to the mission user's svn repo. """
+        open(self.file_to_patch, 'w').write(self.proposed_content)
+        commit_message = "Fix a typo in {0:s}. Thanks for reporting this, {1:s}!".format(self.FILE_TO_BE_PATCHED,
+                                                                                         self.username)
+        snv_commit_command = ['svn', 'commit', '-m', commit_message, '--username', 'mr_bad', self.working_directory]
+        view_helpers.subproc_check_output(snv_commit_command)
